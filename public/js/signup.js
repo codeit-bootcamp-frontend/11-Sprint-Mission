1bcbe97f1d6040732eca65175054ef32a7a9e61b
WooLegend <dnwogus4260@naver.com>
2024-09-10 03:43:58 +0900

    [fix] edit visibility btn interaction

--- public/js/signup.js
+++ public/js/signup.js
@@ -42,12 +42,12 @@ visibleBtn1.addEventListener('click', function (e) {
 
   if (type === 'password') {
     inputPassword.setAttribute('type', 'text');
-    eye.classList.remove('active');
-    eyeSlash.classList.add('active');
-  } else if (type === 'text') {
-    inputPassword.setAttribute('type', 'password');
     eye.classList.add('active');
     eyeSlash.classList.remove('active');
+  } else if (type === 'text') {
+    inputPassword.setAttribute('type', 'password');
+    eye.classList.remove('active');
+    eyeSlash.classList.add('active');
   }
 })
 
