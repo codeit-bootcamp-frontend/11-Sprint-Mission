777b67d65328b8636f1f7bde3d5ba58e45b1f0f1
WooLegend <dnwogus4260@naver.com>
2024-09-10 02:08:54 +0900

    [style] change login.js style

--- public/js/login.js
+++ public/js/login.js
@@ -1,18 +1,18 @@
 const inputPassword = document.querySelector('#input-password')
+const visibleBtn = document.querySelector('.visible-btn')
 
-document.querySelector('.visible-btn')
-  .addEventListener('click', function (e) {
-    const type = inputPassword.getAttribute('type');
-    const eye = document.querySelector('.fa-eye');
-    const eyeSlash = document.querySelector('.fa-eye-slash');
+visibleBtn.addEventListener('click', function (e) {
+  const type = inputPassword.getAttribute('type');
+  const eye = visibleBtn.querySelector('.fa-eye');
+  const eyeSlash = visibleBtn.querySelector('.fa-eye-slash');
 
-    if(type === 'password'){
-      inputPassword.setAttribute('type', 'text');
-      eye.classList.remove('active');
-      eyeSlash.classList.add('active');
-    }else if(type === 'text'){
-      inputPassword.setAttribute('type', 'password');
-      eye.classList.add('active');
-      eyeSlash.classList.remove('active');
-    }
-  })
+  if (type === 'password') {
+    inputPassword.setAttribute('type', 'text');
+    eye.classList.remove('active');
+    eyeSlash.classList.add('active');
+  } else if (type === 'text') {
+    inputPassword.setAttribute('type', 'password');
+    eye.classList.add('active');
+    eyeSlash.classList.remove('active');
+  }
+})
