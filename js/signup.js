4a31b2ca82c2326fd91c58424205e10531dafb61
mangmang1004 <100568101+mangmang1004@users.noreply.github.com>
2024-09-14 22:17:43 +0900

    ⚡️ Signup 비밀번호 visibility 버튼 연결하기
    signup 비밀번호 visibility 버튼에 JS 연결하기

--- js/signup.js
+++ js/signup.js
@@ -37,6 +37,37 @@ signupEmail.addEventListener('input', inputSignupEmail);
 signupNickname.addEventListener('input', inputSignupNickname);
 signupPassword.addEventListener('input', inputSignupPassword);
 signupCheckPassword.addEventListener('input', inputSignupCheckPassword);
-signupBtn.addEventListener('click')
+//signupBtn.addEventListener('click')
 
 
+let SignupPasswordVisibility = document.querySelector('.signup-password-visibility');
+let SignupCheckpasswordVisibility = document.querySelector('.signup-checkpassword-visibility');
+
+console.log(SignupPasswordVisibility);
+
+
+function setSignupPasswordVisibility() {
+  let img = SignupPasswordVisibility.getAttribute('src');
+  if(img === './img/btn_visibility_off.svg'){
+    SignupPasswordVisibility.setAttribute('src', './img/btn_visibility_on.svg');
+    SignupPasswordVisibility.previousElementSibling.setAttribute('type', 'text');
+  } else {
+    SignupPasswordVisibility.setAttribute('src', './img/btn_visibility_off.svg');
+    SignupPasswordVisibility.previousElementSibling.setAttribute('type', 'password');
+  }
+}
+
+function setSignupCheckpasswordVisibility() {
+  let img = SignupCheckpasswordVisibility.getAttribute('src');
+  if(img === './img/btn_visibility_off.svg'){
+    SignupCheckpasswordVisibility.setAttribute('src', './img/btn_visibility_on.svg');
+    SignupCheckpasswordVisibility.previousElementSibling.setAttribute('type', 'text');
+  } else {
+    SignupCheckpasswordVisibility.setAttribute('src', './img/btn_visibility_off.svg');
+    SignupCheckpasswordVisibility.previousElementSibling.setAttribute('type', 'password');
+  }
+}
+
+
+SignupPasswordVisibility.addEventListener('click', setSignupPasswordVisibility);
+SignupCheckpasswordVisibility.addEventListener('click', setSignupCheckpasswordVisibility);
