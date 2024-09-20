38d84eb87c20a86fdf663be75cabf5f93c6701b4
1022gusl <1022gusl@gmail.com>
2024-09-21 03:57:30 +0900

    add form_error and basic js code

---
+++ sign.js
@@ -0,0 +1,12 @@
+const emailText = document.getElementById("email");
+const usernameText = document.getElementById("username");
+const passwordText = document.getElementById("password");
+const passwordRepeatText = document.getElementById("password_repeat");
+
+function show () {
+    input.style.border = "red";
+}
+
+function hide () {
+    input.style.border = "none";
+}
