0ea276c8371340695a57a78014df8ac32c6b050a
WooLegend <dnwogus4260@naver.com>
2024-09-21 01:13:30 +0900

    [Refactor] togglePasswordVisibility 함수 리팩토링
    login.js와 signup.js 파일에 존재하는 함수 togglePasswordVisivility에 대한 변경점
    1. 각 명령에 주석 추가
    2. 비밀번호 표기 버튼의 스타일 토글 코드 간소화
    	- this.children의 타입을 유사 배열에서 배열로 변경
    	- for 반복문에서 forEach로 변경

--- public/js/signup.js
+++ public/js/signup.js
@@ -26,15 +26,16 @@ function checkAllInputFill() {
 // }
 
 // 비밀번호 표기 토글 이벤트 핸들러
-function togglePasswordVisibility(event) {
+function togglePasswordVisibility() {
+  // password input tag
   const inputTag = this.previousElementSibling;
   const type = inputTag.getAttribute('type') === 'password' ? 'text' : 'password';
 
-  // 비밀번호(확인) 입력란을 가리킴
+  // 비밀번호 입력란을 가리킴
   inputTag.setAttribute('type', type);
-  for (const icon of this.children) {
-    icon.classList.toggle('hide');
-  }
+
+  // 비밀번호 표기 버튼 토글
+  Array.from(this.children).forEach(e => e.classList.toggle('hide'))
 }
 
 document.querySelectorAll('.visibility-btn').forEach(btn => {
