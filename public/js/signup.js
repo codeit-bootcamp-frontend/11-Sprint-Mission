d06957762bb664ba1b34835159b589573c813ccd
WooLegend <dnwogus4260@naver.com>
2024-09-21 00:59:52 +0900

    [Refactor] input-password-repeat 핸들링 일시 비활성화
    signup.js
    1. 이벤트리스너 비활성화
    2. 핸들러 비황성화
    	- comparePassword

--- public/js/signup.js
+++ public/js/signup.js
@@ -9,21 +9,22 @@ function checkAllInputFill() {
   submitBtn.classList.toggle('active', isAllFilled);
 }
 
-function comparePassword() {
-  const alert = document.querySelector('.repeat-alert');
-  const pw = inputTags[2].value;
-  const rpw = inputTags[3].value;
+// function comparePassword() {
+//   const alert = document.querySelector('.repeat-alert');
+//   const pw = inputTags[2].value;
+//   const rpw = inputTags[3].value;
 
-  if (pw === rpw) {
-    alert.textContent = '비밀번호와 일치합니다.';
-    alert.classList.add('correct');
-    alert.classList.remove('warning');
-  } else {
-    alert.textContent = '비밀번호와 일치하지 않습니다.';
-    alert.classList.remove('correct');
-    alert.classList.add('warning');
-  }
-}
+//   if (pw === rpw) {
+//     alert.textContent = '비밀번호와 일치합니다.';
+//     alert.classList.add('correct');
+//     alert.classList.remove('warning');
+//   } else {
+//     alert.textContent = '비밀번호와 일치하지 않습니다.';
+//     alert.classList.remove('correct');
+//     alert.classList.add('warning');
+//   }
+// }
+
 // 비밀번호 표기 토글 이벤트 핸들러
 function togglePasswordVisibility(event) {
   const inputTag = this.previousElementSibling;
@@ -40,5 +41,5 @@ document.querySelectorAll('.visibility-btn').forEach(btn => {
   btn.addEventListener('click', togglePasswordVisibility)
 })
 document.addEventListener('input', checkAllInputFill)
-document.querySelector('#input-password-repeat')
-  .addEventListener('change', comparePassword)
+// document.querySelector('#input-password-repeat')
+//   .addEventListener('change', comparePassword)
