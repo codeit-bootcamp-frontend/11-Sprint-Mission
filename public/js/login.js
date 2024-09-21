e9144cab7b7b6d4cd3d421939558ff7699bf2a1d
WooLegend <dnwogus4260@naver.com>
2024-09-22 05:56:59 +0900

    [Feat] 로그인 버튼 활성화/비활성화 제어 함수 추가
    - checkAllInputFill함수를 checkAllInputValid로 대체
    - validInputs 객체로 입력값 유효 여부 관리
    - checkAllInputValid가 validInputs을 바탕으로 모든 입력 유효 판단
    - submitForm함수로 폼 제출 여부 판단

--- public/js/login.js
+++ public/js/login.js
@@ -1,13 +1,4 @@
-// 로그인에 필요한 필수 입력 요소를 모두 채웠는지 확인하는 함수
-function checkAllInputFill() {
-  const inputTags = document.querySelectorAll('#login-form input:required')
-  const submitBtn = document.querySelector('#login-submit-btn');
-
-  let isAllFilled = Array.from(inputTags).every(tag =>
-    tag.value.trim() !== '');
-
-  submitBtn.classList.toggle('active', isAllFilled);
-}
+const validInputs = {email : false, password : false}
 
 // 입력 이메일 형식 확인 함수
 function checkEmailFormat() {
@@ -23,6 +14,7 @@ function checkEmailFormat() {
     this.classList.add('warning');
     alert.classList.add('warning');
     alert.textContent = '이메일을 입력해주세요.';
+    validInputs.email = false;
     return;
   }
 
@@ -31,6 +23,7 @@ function checkEmailFormat() {
     this.classList.add('warning');
     alert.classList.add('warning');
     alert.textContent = '잘못된 이메일 형식입니다.';
+    validInputs.email = false;
     return;
   }
 
@@ -38,6 +31,7 @@ function checkEmailFormat() {
   this.classList.remove('warning');
   alert.classList.remove('warning');
   alert.textContent = '';
+  validInputs.email = true;
 }
 
 // 입력 비밀번호 형식 확인 함수
@@ -53,6 +47,7 @@ function checkPasswordFormat() {
     this.classList.add('warning');
     alert.classList.add('warning');
     alert.textContent = '비밀번호를 입력해주세요.';
+    validInputs.password = false;
     return;
   }
 
@@ -61,6 +56,7 @@ function checkPasswordFormat() {
     this.classList.add('warning');
     alert.classList.add('warning');
     alert.textContent = '비밀번호를 8자 이상 입력해주세요.';
+    validInputs.password = false;
     return;
   }
 
@@ -68,6 +64,7 @@ function checkPasswordFormat() {
   this.classList.remove('warning');
   alert.classList.remove('warning');
   alert.textContent = '';
+  validInputs.password = true;
 }
 
 // 비밀번호 표기 토글 이벤트 핸들러
@@ -83,14 +80,48 @@ function togglePasswordVisibility() {
   Array.from(this.children).forEach(e => e.classList.toggle('hide'))
 }
 
-// 입력 이메일 형식 확인 이벤트
-document.querySelector('#input-email').addEventListener('focusout', checkEmailFormat)
+/**
+ * 로그인 폼의 모든 입력이 유효한지 확인하는 함수
+ * @return {Boolean} 유효 여부 반환
+ * @desctiption 모든 입력의 유효 여부를 확인한다. 유효할 때 로그인 버튼을 활성화 스타일로 변경하고 아니면 비활성화 스타일로 변경한다.
+ */
+function checkAllInputValid() {
+  const submitBtn = document.querySelector('#login-submit-btn');
+  const result = Object.keys(validInputs).every(key => validInputs[key]);
 
-// 입력 비밀번호 형식 확인 이벤트
-document.querySelector('#input-password').addEventListener('focusout', checkPasswordFormat)
+  submitBtn.classList.toggle('active', result);
+  return result;
+}
 
-// 비밀번호 표기 버튼 토글 이벤트
+/**
+ * 폼 제출을 제어하는 함수
+ * @param {Event} e 로그인 폼의 제출 이벤트
+ * @description 폼의 모든 입력이 유효할 때 폼을 제출하고 아니면 제출을 막는다. 현재는 폼을 제출하는 대신 /items로 이동고 그렇지 않으면 경고팝업을 띄운다.
+ */
+function submitForm(e){
+  e.preventDefault();
+  const result = checkAllInputValid();
+
+  if(result) location.href = '/views/items.html';
+  else alert('잘못된 접근입니다.');
+}
+
+
+// 입력 이메일 형식 확인
+document.querySelector('#input-email')
+  .addEventListener('focusout', checkEmailFormat);
+
+// 입력 비밀번호 형식 확인
+document.querySelector('#input-password')
+  .addEventListener('focusout', checkPasswordFormat);
+
+// 모든 입력값이 형식에 맞는지 확인
+document.querySelector('#login-form')
+  .addEventListener('focusout', checkAllInputValid);
+
+document.querySelector('#login-form')
+  .addEventListener('submit', submitForm);
+
+// 비밀번호 표기 버튼 토글
 document.querySelector('.visibility-btn')
-  .addEventListener('click', togglePasswordVisibility)
-
-document.addEventListener('input', checkAllInputFill)
+  .addEventListener('click', togglePasswordVisibility);
