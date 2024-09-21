28b984b3261312cf77943e37ecaca3235364e740
WooLegend <dnwogus4260@naver.com>
2024-09-22 05:01:34 +0900

    [Feat] 비밀번호 형식 검증 함수 추가

--- public/js/login.js
+++ public/js/login.js
@@ -9,6 +9,7 @@ function checkAllInputFill() {
   submitBtn.classList.toggle('active', isAllFilled);
 }
 
+// 입력 이메일 형식 확인 함수
 function checkEmailFormat() {
   // input tag를 포함하는 fieldset
   const fieldset = this.parentNode;
@@ -26,13 +27,44 @@ function checkEmailFormat() {
   }
 
   // 잘못된 형식의 이메일
-  if(!regex.test(email)){
+  if (!regex.test(email)) {
     this.classList.add('warning');
     alert.classList.add('warning');
     alert.textContent = '잘못된 이메일 형식입니다.';
     return;
   }
 
+  // 문제 없음
+  this.classList.remove('warning');
+  alert.classList.remove('warning');
+  alert.textContent = '';
+}
+
+// 입력 비밀번호 형식 확인 함수
+function checkPasswordFormat() {
+  // input tag를 포함하는 fieldset
+  const fieldset = this.parentNode;
+  // input과 관련된 문구를 삽입할 p태그
+  const alert = fieldset.querySelector('.input-alert');
+  const password = this.value.trim();
+
+  // 비밀번호 미입력
+  if (password === '') {
+    this.classList.add('warning');
+    alert.classList.add('warning');
+    alert.textContent = '비밀번호를 입력해주세요.';
+    return;
+  }
+
+  // 짧은 비밀번호
+  if (password.length < 8) {
+    this.classList.add('warning');
+    alert.classList.add('warning');
+    alert.textContent = '비밀번호를 8자 이상 입력해주세요.';
+    return;
+  }
+
+  // 문제 없음
   this.classList.remove('warning');
   alert.classList.remove('warning');
   alert.textContent = '';
@@ -51,8 +83,11 @@ function togglePasswordVisibility() {
   Array.from(this.children).forEach(e => e.classList.toggle('hide'))
 }
 
-// 입력 이메일 포맷 확인 이벤트
+// 입력 이메일 형식 확인 이벤트
 document.querySelector('#input-email').addEventListener('focusout', checkEmailFormat)
+
+// 입력 비밀번호 형식 확인 이벤트
+document.querySelector('#input-password').addEventListener('focusout', checkPasswordFormat)
 
 // 비밀번호 표기 버튼 토글 이벤트
 document.querySelector('.visibility-btn')
