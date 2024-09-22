0dce494496131f4a96891507eb5242e7d89ae77b
WooLegend <dnwogus4260@naver.com>
2024-09-22 20:08:11 +0900

    [Feat] 회원가입 페이지 구조 변경 및 불필요 기능 삭제
    구조 변경
    - 입력란 필드셋으로 감쌈
      - 이메일
      - 닉네임
    - 입력에 대한 경고 삽입을 위한 요소 추가
      - 이메일
      - 닉네임
      - 비밀번호
      - 비밀번호 확인
    
    기능 삭제
    - 필드 확인 함수 삭제
    - 폼 이벤트 리스너 삭제

--- public/js/signup.js
+++ public/js/signup.js
@@ -1,13 +1,10 @@
-// 로그인에 필요한 필수 입력 요소를 모두 채웠는지 확인하는 함수
-function checkAllInputFill() {
-  const inputTags = document.querySelectorAll('#signup-form input:required')
-  const submitBtn = document.querySelector('#signup-submit-btn');
-
-  let isAllFilled = Array.from(inputTags).every(tag => 
-    tag.value.trim() !== '');
-
-  submitBtn.classList.toggle('active', isAllFilled);
+const validInputs = {
+  email: false,
+  nickname :false,
+  password: false,
+  passwordRepeat : false
 }
+
 
 // 비밀번호 표기 토글 이벤트 핸들러
 function togglePasswordVisibility() {
@@ -26,5 +23,3 @@ function togglePasswordVisibility() {
 document.querySelectorAll('.visibility-btn').forEach(btn => {
   btn.addEventListener('click', togglePasswordVisibility)
 })
-
-document.addEventListener('input', checkAllInputFill)
