131f9f685d4ecda16f7836af6e4a4ea0b595d2cd
SeokJiwoo <jiwoo9665@gmail.com>
2024-09-24 21:26:14 +0900

    ✨ 비밀번호 8자 미만일 경우와 확인input 값이 다른 경우 추가

--- loginSignup.js
+++ loginSignup.js
@@ -1,4 +1,4 @@
-
+// 이메일
 const email = document.querySelector('#email');
 const emailNull = document.querySelector('.email-null');
 const emailCheck = document.querySelector('.email-check');
@@ -30,18 +30,49 @@ email.addEventListener('blur', function() {
 });
 
 
+// 비밀번호
 const password = document.querySelector('#password');
+
 const passwordNull = document.querySelector('.password-null');
+const passwordCheck = document.querySelector('.password-check');
+
 password.addEventListener('blur',function(){
+
+    //   값이 없을 경우
     if(!password.value){
         password.classList.add('error');
         passwordNull.style.display = 'block';
-    }else{
+    }
+
+    // 비밀번호가 8자 미만
+    else if(password.value.length < 8){
+            password.classList.add('error');
+            passwordCheck.style.display = 'block';
+        }
+    
+    else{
         password.classList.remove('error');
         passwordNull.style.display = 'none';
+        passwordCheck.style.display = 'none';
     }
 }); 
 
+// 비밀번호 확인 일치하지 않는 경우
+const passwordVerify = document.querySelector('#password-verify');
+const passwordDifferent = document.querySelector('.password-different')
+
+passwordVerify.addEventListener('blur',function(){
+    if(password.value != passwordVerify.value){
+        passwordVerify.classList.add('error');
+        passwordDifferent.style.display = 'block';
+    } else{
+        passwordVerify.classList.remove('error');
+        passwordDifferent.style.display = 'none';
+    }
+});
+
+
+// 닉네임
 const username = document.querySelector('#username');
 const usernameNull = document.querySelector('.username-null');
 username.addEventListener('blur',function(){
