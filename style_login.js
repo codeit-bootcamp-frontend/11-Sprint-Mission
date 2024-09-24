02c63a9257f969c56d4bf7645588cf92abafcb27
ksj <naver.daum0711@gmail.com>
2024-09-24 14:11:36 +0900

    1st commit

---
+++ style_login.js
@@ -0,0 +1,14 @@
+// 버튼 클릭 효과 //
+// 눈 모양 //
+
+// const loginButton = document.querySelector('.login_button');
+// const signupButton = document.querySelector('.signup_button');
+// const inputTags = document.querySelectorAll('input');
+
+// const checkInputs = () => {
+//   const isFilled = [...inputTags].every(input => input.value.trim() !== '');
+//   loginButton.classList.toggle('active', isFilled);
+//   signupButton.classList.toggle('active', isFilled);
+// };
+
+// inputTags.forEach(input => input.addEventListener('input', checkInputs));
