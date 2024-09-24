18822a0098c4f07fd20918287121ba86bc261c21
SeokJiwoo <jiwoo9665@gmail.com>
2024-09-24 20:35:42 +0900

    ✨ 이메일 형식 체크 추가

--- loginSignup.js
+++ loginSignup.js
@@ -1,19 +1,37 @@
-const email = document.querySelector('#email');
-const emailNull = document.querySelector('.email-null')
 
-email.addEventListener('blur', function(){
-    if(!email.value){
-        email.classList.add('error');
-        emailNull.style.display = 'block';
-    }else{
-        email.classList.remove('error');
-        emailNull.style.display = 'none';
-    }
+const email = document.querySelector('#email');
+const emailNull = document.querySelector('.email-null');
+const emailCheck = document.querySelector('.email-check');
+
+const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+email.addEventListener('blur', function() {
+  const emailValue = email.value;
+
+//   값이 없을 경우
+  if (!emailValue) {
+    email.classList.add('error');
+    emailNull.style.display = 'block';
+    emailCheck.style.display = 'none';
+  } 
+
+//   이메일 형식에 맞지 않는 경우
+  else if (!emailPattern.test(emailValue)) {
+    email.classList.add('error');
+    emailNull.style.display = 'none';
+    emailCheck.style.display = 'block';
+  } 
+
+  else {
+    email.classList.remove('error');
+    emailNull.style.display = 'none';
+    emailCheck.style.display = 'none';
+  }
 });
+
 
 const password = document.querySelector('#password');
 const passwordNull = document.querySelector('.password-null');
-
 password.addEventListener('blur',function(){
     if(!password.value){
         password.classList.add('error');
@@ -26,7 +44,6 @@ password.addEventListener('blur',function(){
 
 const username = document.querySelector('#username');
 const usernameNull = document.querySelector('.username-null');
-
 username.addEventListener('blur',function(){
     if(!username.value){
         username.classList.add('error');
@@ -36,3 +53,4 @@ username.addEventListener('blur',function(){
         usernameNull.style.display = 'none';
     }
 });
+
