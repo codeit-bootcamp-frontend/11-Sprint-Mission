79580bb1ce14d16124d2d36a03589099fefa0375
SeokJiwoo <jiwoo9665@gmail.com>
2024-09-25 16:56:36 +0900

    ✨ 로그인과 회원가입 js 파일 분리, 비밀번호 눈 기능 추가

--- login.js
+++ login.js
@@ -8,36 +8,23 @@ const password = document.querySelector('#password');
 const passwordNull = document.querySelector('.password-null');
 const passwordCheck = document.querySelector('.password-check');
 
-//비밀번호 확인
-const passwordVerify = document.querySelector('#password-verify');
-const passwordDifferent = document.querySelector('.password-different')
-
-//닉네임
-const username = document.querySelector('#username');
-const usernameNull = document.querySelector('.username-null');
-
 // 로그인 버튼
 const loginBtn = document.querySelector('#login-btn');
-
-//회원가입 버튼
-const signupBtn = document.querySelector('#signup-btn')
 
 // 이메일 형식
 const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
-
 // 이메일
 email.addEventListener('input', function() {
-    const emailValue = email.value;
 
     // 값 없는 경우
-    if (!emailValue) {
+    if (!email.value) {
         email.classList.add('error');
         emailNull.style.display = 'block';
         emailCheck.style.display = 'none';
     }
     // 형식에 맞지 않는 경우
-    else if (!emailPattern.test(emailValue)) {
+    else if (!emailPattern.test(email.value)) {
         email.classList.add('error');
         emailNull.style.display = 'none';
         emailCheck.style.display = 'block';
@@ -49,7 +36,6 @@ email.addEventListener('input', function() {
         emailCheck.style.display = 'none';
     }
     toggleLoginButton()
-    toggleSinupButton()
 });
 
 // 비밀번호
@@ -74,7 +60,6 @@ password.addEventListener('input', function() {
         passwordCheck.style.display = 'none';
     }
     toggleLoginButton()
-    toggleSinupButton()
 });
 
 // 로그인 버튼
@@ -91,42 +76,18 @@ function toggleLoginButton() {
     }
 };
 
-// 비밀번호 확인
-passwordVerify.addEventListener('input',function(){
-    if(password.value != passwordVerify.value){
-        passwordVerify.classList.add('error');
-        passwordDifferent.style.display = 'block';
-    } else{
-        passwordVerify.classList.remove('error');
-        passwordDifferent.style.display = 'none';
-    }
-    toggleSinupButton()
+// 비밀번호 눈 아이콘
+const eyeOff = document.querySelector(".eye-off");
+const eyeOn = document.querySelector(".eye-on");
+
+eyeOff.addEventListener('click', function() {
+    password.type = "text"; 
+    eyeOff.style.display = 'none';
+    eyeOn.style.display = 'block';
 });
 
-// 닉네임
-username.addEventListener('input',function(){
-    if(!username.value){
-        username.classList.add('error');
-        usernameNull.style.display = 'block';
-    }else{
-        username.classList.remove('error');
-        usernameNull.style.display = 'none';
-    }
-    toggleSinupButton()
+eyeOn.addEventListener('click', function() {
+    password.type = "password";
+    eyeOn.style.display = 'none';
+    eyeOff.style.display = 'block';
 });
-
-//회원가입 버튼
-function toggleSinupButton() {
-    const isEmailValid = emailPattern.test(email.value); 
-    const isPasswordValid = password.value.length >= 8; 
-    const isPasswordVerifyValid = passwordVerify.value === password.value;
-    const isUsernameValid = username.value.trim() !== "";
-
-    if (isEmailValid && isPasswordValid && isPasswordVerifyValid && isUsernameValid) {
-        signupBtn.classList.add('active');
-        loginBtn.removeAttribute('disabled');
-    } else {
-        signupBtn.classList.remove('active');
-        signupBtn.setAttribute('disabled', 'true');
-    }
-};
