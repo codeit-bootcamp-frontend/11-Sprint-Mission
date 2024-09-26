c4083b12cb3ed5e87818b2a5412d3ab161a3da18
haksoo <haksoo0918@gmail.com>
2024-09-26 19:48:45 +0900

    추가: 폼 요소 확인 절차 및 스타일 추가

---
+++ js/chkLoginForm.js
@@ -0,0 +1,50 @@
+const fmLogin = document.getElementById('form-login');
+const btnLogin = document.getElementById('btn-login');
+//
+const emailReg = /^[A-Za-z0-9_\.\-]+@[A-Za-z0-9\-]+\.[A-za-z0-9\-]+/;
+const isValid = {
+  email: false,
+  pw: false,
+};
+
+// 로그인 버튼 활성화 함수
+const chkBtnLogin = () => {
+  // console.log('fn:chkBtnLogin', isValid);
+  btnLogin.disabled = Object.values(isValid).some(value => value === false);
+}
+
+// 요소 유효 처리 함수
+// msg는 실패한 경우에만 있다는 가정
+const procValid = (el, flag, msg) => {
+  // console.log('fn:procValid');
+  if (!msg) {
+    el.classList.remove('invalid');
+    isValid[flag] = true;
+  } else {
+    el.classList.add('invalid');
+    el.nextElementSibling.textContent = msg;
+    isValid[flag] = false;
+  }
+  chkBtnLogin();
+}
+
+// form에 input focusout 이벤트 위임
+fmLogin.addEventListener('focusout', e => {
+  const el = e.target;
+  const value = el.value.trim();
+
+  // email 확인
+  if (el.id === 'email') {
+    if (!value) return procValid(el, 'email', '이메일을 입력해 주세요.');
+    if (!emailReg.test(value)) return procValid(el, 'email', '잘못된 이메일 형식입니다.');
+    procValid(el, 'email');
+  }
+  // password 확인
+  if (el.id === 'pw') {
+    const minLength = 8;
+
+    if (!value) return procValid(el, 'pw', '비밀번호를 입력해 주세요.');
+    if (value.length < minLength) return procValid(el, 'pw', '비밀번호를 8자 이상 입력해 주세요.');
+    procValid(el, 'pw');
+  }
+});
