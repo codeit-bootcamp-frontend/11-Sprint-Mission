2e7c948b1c03d6293105466d73d6c82d443e4f55
양정화 <siltarre@gmail.com>
2024-09-26 22:31:56 +0900

    리팩토링 : getHeaderRender 함수 병합

--- assets/js/components/headerTemplate.js
+++ assets/js/components/headerTemplate.js
@@ -1,24 +1,21 @@
-function getHeaderTemplate(){
-  return `
-      <div class="container">
-        <h1 class="logo">
-          <a href="/">
-            <picture>
-              <source srcset="assets/images/mobiles/logo.png" media="(max-width: 767px)">
-              <img src="assets/images/logo.png" alt="��다마�� 로고">
-            </picture>
-          </a>
-        </h1>
-        <div class="btn-login">
-          <a href="/login.html" class="button square blue">로그인</a>
-        </div>
-      </div>
-    `;
-}
 function getHeaderRender(){
   const headerEl = document.getElementById('header');
   if(headerEl){
-    headerEl.innerHTML = getHeaderTemplate();
+    headerEl.innerHTML = `
+    <div class="container">
+      <h1 class="logo">
+        <a href="/">
+          <picture>
+            <source srcset="assets/images/mobiles/logo.png" media="(max-width: 767px)">
+            <img src="assets/images/logo.png" alt="��다마�� 로고">
+          </picture>
+        </a>
+      </h1>
+      <div class="btn-login">
+        <a href="/login.html" class="button square blue">로그인</a>
+      </div>
+    </div>
+    `;
   }
 }
 export default getHeaderRender;
