d79bdce67e0ebc76869daed1de1c0a01ce910783
twtwkim <alfog23@gmail.com>
2024-09-27 10:50:37 +0900

    add login/register error funtion

--- module/toggleLoginButton.js
+++ module/toggleLoginButton.js
@@ -1,27 +1,58 @@
-const emailInput = document.querySelector('input[type="email"]');
-const passwordInput = document.querySelector('input[type="password"]');
+const emailInput = document.querySelector('.emailInput');
+const passwordInput = document.querySelector('.passwordInput');
 const loginBtn = document.querySelector('.login-button');
 
-const emailError = document.createElement('p');
-const passwordError = document.createElement('p');
+const emailError = document.querySelector('div.email-error');
+const passwordError = document.querySelector('div.password-error');
 
-function toggleButton() {
-    if (emailInput.value && passwordInput.value) {
+const passwordIcon = document.querySelector('.password-icon')
+
+function toggleButton() { //버튼 활성화
+    if (emailInput.value && passwordInput.value && !emailError.textContent && !passwordError.textContent) {
         loginBtn.classList.add('active');
     } else {
         loginBtn.classList.remove('active')
     }
 }
 
-function focusOut(){
+function focusOut(){ //에러 띄우기
+    //이메일 검증
     if(!emailInput.value){
         emailInput.classList.add('input-error');
         emailError.textContent = "이메일을 입력해주세요.";
-    }else{
+    }else if(!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(emailInput.value)){
+        emailInput.classList.add('input-error');
+        emailError.textContent = "잘못된 이메일 형식입니다.";
+    }
+    else{
+        emailInput.classList.remove('input-error');
         emailError.textContent = "";
     }
+    //비밀번호 검증
+    if(!passwordInput.value){
+        passwordInput.classList.add('input-error');
+        passwordError.textContent = "비밀번호를 입력해주세요.";
+    }else if(passwordInput.value.length<8){
+        passwordInput.classList.add('input-error');
+        passwordError.textContent = "비밀번호를 8자 이상 입력해주세요.";
+    }
+    else{
+        passwordInput.classList.remove('input-error');
+        passwordError.textContent = "";
+    }
+    toggleButton(); //매번 에러 체크할때마다 버튼 상태 체크
+}
+
+function passwordVisibleIcon(){
+    const isVisible = passwordInput.type === 'text';
+    passwordInput.type = isVisible ? 'password' : 'text';
+    passwordIcon.src = isVisible ? './image/gnb/btn_visibility_off_24px.png' : './image/gnb/btn_visibility_on_24px.png'
 }
 
 emailInput.addEventListener('input', toggleButton);
 emailInput.addEventListener('focusout', focusOut);
+
 passwordInput.addEventListener('input', toggleButton);
+passwordInput.addEventListener('focusout', focusOut);
+
+passwordIcon.addEventListener('click', passwordVisibleIcon);
--- module/toggleSignupButton.js
+++ module/toggleSignupButton.js
@@ -4,15 +4,78 @@ const passwordInput = document.querySelector('input[type="password"]');
 const confirmPasswordInput = document.querySelector('input[type="passwordConfirm"]');
 const signBtn = document.querySelector('.sign-button');
 
+const emailError = document.querySelector('div.email-error');
+const usernameError = document.querySelector('div.username-error');
+const passwordError = document.querySelector('div.password-error');
+const passwordConfirmError = document.querySelector('div.passwordConfirm-error');
+
 function toggleSignupButton() {
-    if (emailInput.value && usernameInput.value && passwordInput.value && confirmPasswordInput.value) {
+    if (emailInput.value && usernameInput.value && passwordInput.value && confirmPasswordInput.value && !emailError.textContent && !passwordError.textContent && !usernameError.textContent && !passwordConfirmError.textContent) {
         signBtn.classList.add('active');
     } else {
         signBtn.classList.remove('active')
     }
 }
 
+function focusOut(){ //에러 띄우기
+    //이메일 검증
+    if(!emailInput.value){
+        emailInput.classList.add('input-error');
+        emailError.textContent = "이메일을 입력해주세요.";
+    }else if(!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(emailInput.value)){
+        emailInput.classList.add('input-error');
+        emailError.textContent = "잘못된 이메일 형식입니다.";
+    }
+    else{
+        emailInput.classList.remove('input-error');
+        emailError.textContent = "";
+    }
+    //닉네임 검증
+    if(!usernameInput.value){
+        usernameInput.classList.add('input-error');
+        usernameError.textContent = "닉네임을 입력해주세요.";
+    }else{
+        usernameInput.classList.remove('input-error');
+        usernameError.textContent = "";
+    }
+    //비밀번호 검증
+    if(!passwordInput.value){
+        passwordInput.classList.add('input-error');
+        passwordError.textContent = "비밀번호를 입력해주세요.";
+    }else if(passwordInput.value.length<8){
+        passwordInput.classList.add('input-error');
+        passwordError.textContent = "비밀번호를 8자 이상 입력해주세요.";
+    }
+    else{
+        passwordInput.classList.remove('input-error');
+        passwordError.textContent = "";
+    }
+    //비밀번호 확인 검증
+    if(!confirmPasswordInput.value){
+        confirmPasswordInput.classList.add('input-error');
+        passwordConfirmError.textContent = "비밀번호를 다시 한 번 입력해주세요.";
+    }else if(confirmPasswordInput.value.length<8){
+        confirmPasswordInput.classList.add('input-error');
+        passwordConfirmError.textContent = "비밀번호를 8자 이상 입력해주세요.";
+    }else if(confirmPasswordInput.value !== passwordInput.value){
+        confirmPasswordInput.classList.add('input-error');
+        passwordConfirmError.textContent = "비밀번호가 일치하지 않습니다..";
+    }
+    else{
+        confirmPasswordInput.classList.remove('input-error');
+        passwordConfirmError.textContent = "";
+    }
+    toggleSignupButton(); //매번 에러 체크할때마다 버튼 상태 체크
+}
+
 emailInput.addEventListener('input', toggleSignupButton);
+emailInput.addEventListener('focusout', focusOut);
+
 usernameInput.addEventListener('input', toggleSignupButton);
+usernameInput.addEventListener('focusout', focusOut);
+
 passwordInput.addEventListener('input', toggleSignupButton);
+passwordInput.addEventListener('focusout', focusOut);
+
 confirmPasswordInput.addEventListener('input', toggleSignupButton);
+confirmPasswordInput.addEventListener('focusout', focusOut);
