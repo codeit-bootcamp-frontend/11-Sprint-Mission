48659f3fb04187fce1f9c8e4cf00c4339c85765b
Park-Jongchan <rachachany@naver.com>
2024-09-29 08:56:14 +0900

    docs 프리티어 띄어쓰기 주석 제거 등 문서작업

--- login/js/validation.js
+++ login/js/validation.js
@@ -1,5 +1,3 @@
-//함수 안에 있는 변수 선언을 밖으로 빼면 왜 실행이 안될까요?
-// chat GPT와 제출된 다른 분의 방법을 참조
 const regex = new RegExp('[a-z0-9]+@[a-z]+.[a-z]{2,3}'); 
 const emailInput = document.getElementById('email');
 const emailError = document.getElementById('emailError');
@@ -61,20 +59,17 @@ function comparePasswords() {
 }
 
 const loginButton = document.getElementById('loginButton');
-//값을 밖으로 빼면 로그인 사이트가 함수 실행이 안되고 안으로 넣으면 comparePassword값을 무시
 
 function activationLogin2() {
-
   const emailValue = emailInput.value.trim(); 
   const passwordValue = password.value.trim(); 
   const nicknameValue = nickname.value.trim();
   const comparePasswordValue = comparePassword.value.trim();
-
   if (regex.test(emailValue) &&
     passwordValue.length >= 8 &&
     nicknameValue.length > 1 &&
-    passwordValue === comparePasswordValue //왜? comparePassword값을 입력도 전에 활성화가 되죠? value값이 중복해서 붙으면 안됨.
-  ) { // 이제는 활성화가 안됨. 다른 값은 입력하자 마자 값이 나오는데 왜 nickname 과 comparePassword만 값이 바로 나오지 않지? nickname.addEventListener('input', activationLogin); 이걸 입력하면 됨.
+    passwordValue === comparePasswordValue 
+  ) { 
     loginButton.disabled = false
     loginButton.classList.add('active');
   } else {
