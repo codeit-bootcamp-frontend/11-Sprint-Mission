ca31baed3fc1b47659438a18cda01716e2b34846
lemon1335 <kimjinhui114@gmail.com>
2024-09-29 01:56:20 +0900

    refactoring 순서 정리

--- function/login.js
+++ function/login.js
@@ -1,38 +1,27 @@
-// 이메일값 가져오기
+// 변수선언
 const inputEmail = document.getElementById("input-email");
-const btnAbled = document.getElementById("login-button");
 const inputPassword = document.getElementById("input-password");
 const loginForm = document.getElementById("login-form");
 const validInputs = document.querySelectorAll("[data-valid]");
+const btnAbled = document.getElementById("login-button");
 
 inputEmail.addEventListener("focusout", (e) => printEventType(e, "email"));
+
 inputPassword.addEventListener("focusout", (e) =>
   printEventType(e, "password")
 );
 
-loginForm.addEventListener("focusout", (e) => checkoutBtn(e));
-function checkoutBtn(e) {
-  const result = Array.from(validInputs).every(
-    (e) => e.dataset.valid === "true"
-  );
-  console.log(result + "completed");
-
-  if (result) {
-    btnAbled.disabled = false;
-  }
-  btnAbled.classList.toggle("active", result);
-}
-
-let regex = new RegExp(
-  "([!#-'*+/-9=?A-Z^-~-]+(.[!#-'*+/-9=?A-Z^-~-]+)*|\"([]!#-[^-~ \t]|(\\[\t -~]))+\")@([!#-'*+/-9=?A-Z^-~-]+(.[!#-'*+/-9=?A-Z^-~-]+)*|[[\t -Z^-~]*])"
-);
-
 function printEventType(e, type) {
+  //이메일 유효값 확인
+  let regex = new RegExp(
+    "([!#-'*+/-9=?A-Z^-~-]+(.[!#-'*+/-9=?A-Z^-~-]+)*|\"([]!#-[^-~ \t]|(\\[\t -~]))+\")@([!#-'*+/-9=?A-Z^-~-]+(.[!#-'*+/-9=?A-Z^-~-]+)*|[[\t -Z^-~]*])"
+  );
   //변수 선언
   const invalidType = e.target.value;
   const parentElement = e.target.parentElement;
   const markText = parentElement.querySelector(".invalid-text");
 
+  //이메일 값 확인
   if (type === "email") {
     if (invalidType === "") {
       markText.textContent = "이메일을 입력해주세요.";
@@ -50,6 +39,7 @@ function printEventType(e, type) {
     inputEmail.classList.remove("invalid-mark");
   }
 
+  // 비밀번호 값 확인
   if (type === "password") {
     if (invalidType === "") {
       markText.textContent = "비밀번호를 입력해주세요.";
@@ -65,6 +55,21 @@ function printEventType(e, type) {
     e.target.dataset.valid = true;
     markText.textContent = "";
     inputPassword.classList.remove("invalid-mark");
+  }
+
+  // 버튼 활성화
+  loginForm.addEventListener("focusout", (e) => checkoutBtn(e));
+
+  function checkoutBtn(e) {
+    const result = Array.from(validInputs).every(
+      (e) => e.dataset.valid === "true"
+    );
+    console.log(result + "completed");
+
+    if (result) {
+      btnAbled.disabled = false;
+    }
+    btnAbled.classList.toggle("active", result);
   }
 
   e.target.dataset.valid = true;
--- function/signup.js
+++ function/signup.js
@@ -1,43 +1,36 @@
+// 변수선언
 const inputEmail = document.getElementById("signup-email");
 const inputNickname = document.getElementById("signup-nickname");
-const btnAbled = document.getElementById("signup-button");
 const inputPassword = document.getElementById("signup-password");
 const checkoutPassword = document.getElementById("checkout-password");
 const signupForm = document.getElementById("signup-form");
 const validInputs = document.querySelectorAll("[data-valid]");
+const btnAbled = document.getElementById("signup-button");
 
 inputEmail.addEventListener("focusout", (e) => printEventType(e, "email"));
+
 inputPassword.addEventListener("focusout", (e) =>
   printEventType(e, "password")
 );
+
 inputNickname.addEventListener("focusout", (e) => printEventType(e, "text"));
+
 checkoutPassword.addEventListener("focusout", (e) =>
   printEventType(e, "password")
 );
 
-signupForm.addEventListener("focusout", (e) => checkoutBtn(e));
-function checkoutBtn(e) {
-  const result = Array.from(validInputs).every(
-    (e) => e.dataset.valid === "true"
-  );
-  console.log(result + "completed");
-
-  if (result) {
-    btnAbled.disabled = false;
-  }
-  btnAbled.classList.toggle("active", result);
-}
-
-let regex = new RegExp(
-  "([!#-'*+/-9=?A-Z^-~-]+(.[!#-'*+/-9=?A-Z^-~-]+)*|\"([]!#-[^-~ \t]|(\\[\t -~]))+\")@([!#-'*+/-9=?A-Z^-~-]+(.[!#-'*+/-9=?A-Z^-~-]+)*|[[\t -Z^-~]*])"
-);
-
 function printEventType(e, type) {
+  //이메일 유효값 확인
+  let regex = new RegExp(
+    "([!#-'*+/-9=?A-Z^-~-]+(.[!#-'*+/-9=?A-Z^-~-]+)*|\"([]!#-[^-~ \t]|(\\[\t -~]))+\")@([!#-'*+/-9=?A-Z^-~-]+(.[!#-'*+/-9=?A-Z^-~-]+)*|[[\t -Z^-~]*])"
+  );
+
   //변수 선언
   const invalidType = e.target.value;
   const parentElement = e.target.parentElement;
   const markText = parentElement.querySelector(".invalid-text");
 
+  //이메일 값 확인
   if (type === "email") {
     if (invalidType === "") {
       markText.textContent = "이메일을 입력해주세요.";
@@ -55,6 +48,7 @@ function printEventType(e, type) {
     inputEmail.classList.remove("invalid-mark");
   }
 
+  //닉네임 값 확인
   if (type === "text") {
     if (invalidType === "") {
       markText.textContent = "닉네임을 입력해주세요";
@@ -67,6 +61,7 @@ function printEventType(e, type) {
     inputNickname.classList.remove("invalid-mark");
   }
 
+  //비밀번호값 확인
   if (type === "password") {
     if (invalidType === "") {
       markText.textContent = "비밀번호를 입력해주세요.";
@@ -84,6 +79,7 @@ function printEventType(e, type) {
     inputPassword.classList.remove("invalid-mark");
   }
 
+  //비밀번호 일치 확인
   if (type === "password") {
     if (invalidType !== inputPassword.value) {
       markText.textContent = "비밀번호가 일치하지 않습니다..";
@@ -94,6 +90,21 @@ function printEventType(e, type) {
     e.target.dataset.valid = true;
     markText.textContent = "";
     checkoutPassword.classList.remove("invalid-mark");
+  }
+
+  //버튼 활성화
+  signupForm.addEventListener("focusout", (e) => checkoutBtn(e));
+
+  function checkoutBtn(e) {
+    const result = Array.from(validInputs).every(
+      (e) => e.dataset.valid === "true"
+    );
+    console.log(result + "completed");
+
+    if (result) {
+      btnAbled.disabled = false;
+    }
+    btnAbled.classList.toggle("active", result);
   }
 
   e.target.dataset.valid = true;
