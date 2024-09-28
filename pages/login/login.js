1e161b9259f7e5dc4a8754b7f164818f5e81853b
Lilly1757 <seungji34@gmail.com>
2024-09-28 14:18:52 +0900

    Refactor: emailInput, paswordInput 중복코드 함수처리

--- pages/login/login.js
+++ pages/login/login.js
@@ -10,49 +10,49 @@ const loginButton = get("#login_button");
 const passwordToggle = get("#password_toggle");
 const inputsError = get("#error")
 
-function changeBorderErrorStatus(element) {
-  element.style.border = "2px solid red";
-}
 
-function innerTextContent(element, text) {
-  element.textContent = text
-}
-
-function checkEmail(email) {
+function checkEmailMessage(email) {
   const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-  return emailPattern.test(email);
+  if (!email) {
+    inputsError[0].textContent = "이메일을 입력해주세요."
+  } else if (!emailPattern.test(email)) {
+    inputsError[0].textContent = "잘못된 이메일 형식입니다."
+  } else {
+    inputsError[0].textContent = ""
+  }
 }
 
-function clearError(inputElement, errorElement) {
-  innerTextContent(errorElement, "");
-  inputElement.style.border = "none";
+function checkPasswordMessage(password) {
+  
+  if (!password) {
+    inputsError[1].textContent = "비밀번호를 입력해주세요."
+  } else if (password.length < 8) {
+    inputsError[1].textContent = "비밀번호를 8자 이상 입력해주세요."
+  } else {
+    inputsError[1].textContent = ""
+  }
 }
- 
+
+function changeBorderErrorStatus(condition, element) {
+  if(condition) {
+    element.style.border = "2px solid red";
+  } else {
+    element.style.border = "none";
+  }
+}
 
 emailInput.addEventListener("focusout", (event) => {
   const { value } = event.target;
+  const errorEmail = !value && !checkEmail(value)
 
-  if (!value) {
-    innerTextContent(inputsError[0], "이메일을 입력해주세요.")
-    changeBorderErrorStatus(emailInput)
-  } else if (!checkEmail(value)) {
-    innerTextContent(inputsError[0], "잘못된 이메일 형식입니다.")
-    changeBorderErrorStatus(emailInput)
-  } else {
-    clearError(emailInput, inputsError[0])
-  }
+  checkEmailMessage(value)
+  changeBorderErrorStatus(errorEmail, emailInput)
 })
 
 paswordInput.addEventListener("focusout", (event) => {
   const { value } = event.target;
+  const errorPassword = !value && value.length < 8
 
-  if (!value) {
-    innerTextContent(inputsError[1], "비밀번호를 입력해주세요.")
-    changeBorderErrorStatus(paswordInput)
-  } else if (value.length < 8) {
-    innerTextContent(inputsError[1], "비밀번호를 8자 이상 입력해주세요.")
-    changeBorderErrorStatus(paswordInput)
-  } else {
-    clearError(paswordInput, inputsError[1])
-  }
+  checkPasswordMessage(value)
+  changeBorderErrorStatus(errorPassword, paswordInput)
 })
