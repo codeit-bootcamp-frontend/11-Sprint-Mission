d1e584ad85dce750f436e318ba60d3fcac7c9b95
Lilly1757 <seungji34@gmail.com>
2024-09-30 00:11:14 +0900

    refactor: lint 처리

--- pages/login/login.js
+++ pages/login/login.js
@@ -2,54 +2,56 @@ const get = (id) => {
   const element = document.querySelectorAll(id);
 
   return element.length === 1 ? element[0] : element;
-}
+};
 
 const emailInput = get("#email");
 const paswordInput = get("#password");
 const loginButton = get("#login_button");
 const passwordToggle = get("#password_toggle");
-const inputsError = get("#error")
+const inputsError = get("#error");
 
-
-function vaildEmail (email) {
-     const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-     return email && emailPattern.test(email);
-   }
-
-function ValidPassword(password) {
-  return password && password.length >= 8;
+function vaildEmail(email) {
+  const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return emailPattern.test(email);
 }
 
+function validPassword(password) {
+  return password.length >= 8;
+}
 
 function vaildateEmail(email) {
+  const emailErrorElement = inputsError[0];
+
   if (!email) {
-    inputsError[0].textContent = "이메일을 입력해주세요."
+    emailErrorElement.textContent = "이메일을 입력해주세요.";
   } else if (!vaildEmail(email)) {
-    inputsError[0].textContent = "잘못된 이메일 형식입니다."
+    emailErrorElement.textContent = "잘못된 이메일 형식입니다.";
   } else {
-    inputsError[0].textContent = ""
+    emailErrorElement.textContent = "";
   }
 }
 
 function vaildatePassword(password) {
+  const passwordErrorElement = inputsError[1];
+
   if (!password) {
-    inputsError[1].textContent = "비밀번호를 입력해주세요."
-  } else if (password.length < 8) {
-    inputsError[1].textContent = "비밀번호를 8자 이상 입력해주세요."
+    passwordErrorElement.textContent = "비밀번호를 입력해주세요.";
+  } else if (!validPassword(password)) {
+    passwordErrorElement.textContent = "비밀번호를 8자 이상 입력해주세요.";
   } else {
-    inputsError[1].textContent = ""
+    passwordErrorElement.textContent = "";
   }
 }
 
 function changeBorderErrorStatus(condition, element) {
-    element.style.border = condition ? "2px solid red" : "none";
+  element.style.border = condition ? "2px solid red" : "none";
 }
 
-function updateLoginButtonStatus (){
+function updateLoginButtonStatus() {
   const emailValue = emailInput.value;
   const passwordValue = paswordInput.value;
   const errorEmail = !vaildEmail(emailValue);
-  const errorPassword = !ValidPassword(passwordValue);
+  const errorPassword = !validPassword(passwordValue);
   return errorEmail || errorPassword;
 }
 
@@ -57,25 +59,25 @@ emailInput.addEventListener("focusout", (event) => {
   const { value } = event.target;
   const errorEmail = !vaildEmail(value);
 
-  vaildateEmail(value)
-  changeBorderErrorStatus(errorEmail, emailInput)
-})
+  vaildateEmail(value);
+  changeBorderErrorStatus(errorEmail, emailInput);
+});
 
 paswordInput.addEventListener("focusout", (event) => {
   const { value } = event.target;
-  const errorPassword = !ValidPassword(value);
+  const errorPassword = !validPassword(value);
 
-  vaildatePassword(value)
-  changeBorderErrorStatus(errorPassword, paswordInput)
-})
+  vaildatePassword(value);
+  changeBorderErrorStatus(errorPassword, paswordInput);
+});
 
-loginButton.addEventListener("mouseover", (event) => {
-  loginButton.disabled = updateLoginButtonStatus ()
-})
+loginButton.addEventListener("mouseover", () => {
+  loginButton.disabled = updateLoginButtonStatus();
+});
 
 loginButton.addEventListener("click", (event) => {
   event.preventDefault();
-  if (updateLoginButtonStatus () === false) {
-    location.href = "../items/items.html"
-  } 
-})
+  if (updateLoginButtonStatus() === false) {
+    location.href = "../items/items.html";
+  }
+});
