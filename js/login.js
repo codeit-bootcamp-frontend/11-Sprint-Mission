6363735114ab3abc0e193dfd5e32a34aa60bbd5f
Hwichul Shin <shinhwiiron@gmail.com>
2024-10-03 19:55:06 +0900

    PR 수정사항 반영

--- js/login.js
+++ js/login.js
@@ -1,32 +1,47 @@
 import {
+  pattern,
   passwordToggleBtn,
   emailInput,
   passwordInput,
   passwordToggleVisibility,
-  emailCheck,
-  passwordCheck,
-  activateLoginBtnState,
-  loginOnEnter
-} from './validation.js';
+  checkEmail,
+  checkPassword,
+} from "./validation.js";
 
-
-// 이메일 형식 체크
-emailInput.addEventListener('focusout', emailCheck);
-emailInput.addEventListener('input', emailCheck);
-
-
-// 비밀번호 체크
-passwordInput.addEventListener('focusout', passwordCheck);
-passwordInput.addEventListener('input', passwordCheck);
-
-
-// 아이콘(눈) 버튼 클릭 시 비밀번호 보이기 / 숨기기
-passwordToggleBtn.addEventListener('click', passwordToggleVisibility);
-
+const loginBtn = document.querySelector(".loginBtn");
 
 // 이메일, 비밀번호 입력 시 로그인 버튼 활성화 / 비활성화
-document.addEventListener('input', activateLoginBtnState);
-
+function activateLoginBtnState() {
+  const isLoginFormValid =
+    pattern.test(emailInput.value.trim()) && passwordInput.value.length >= 8;
+  loginBtn.classList.toggle("activate", isLoginFormValid);
+  loginBtn.disabled = !isLoginFormValid;
+}
 
 // 이메일, 비밀번호 입력 후 엔터키 누르면 로그인 버튼 클릭
-document.addEventListener('keydown', loginOnEnter);
+function loginOnEnter(e) {
+  if (
+    e.keyCode == 13 &&
+    pattern.test(emailInput.value.trim()) &&
+    passwordInput.value.length >= 8
+  ) {
+    loginBtn.click();
+  }
+}
+
+// 이메일 형식 체크
+emailInput.addEventListener("focusout", checkEmail);
+emailInput.addEventListener("input", checkEmail);
+
+// 비밀번호 체크
+passwordInput.addEventListener("focusout", checkPassword);
+passwordInput.addEventListener("input", checkPassword);
+
+// 아이콘(눈) 버튼 클릭 시 비밀번호 보이기 / 숨기기
+passwordToggleBtn.addEventListener("click", passwordToggleVisibility);
+
+// 이메일, 비밀번호 입력 시 로그인 버튼 활성화 / 비활성화
+document.addEventListener("input", activateLoginBtnState);
+
+// 이메일, 비밀번호 입력 후 엔터키 누르면 로그인 버튼 클릭
+document.addEventListener("keydown", loginOnEnter);
