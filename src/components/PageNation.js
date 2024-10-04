ef9a55a8aa9ecb2fdaf3d64a3654af9bee9f6e8a
양정화 <siltarre@gmail.com>
2024-10-05 03:20:04 +0900

    페이지네이션 슬라이딩 윈도우 방식으로 변경

--- src/components/PageNation.js
+++ src/components/PageNation.js
@@ -2,6 +2,7 @@ import "../styles/PageNation/PageNation.css";
 
 const PageNation = ({ total, pageSize, setCurrentPage, currentPage }) => {
   const totalPageNum = Math.ceil(total / pageSize);
+  const maxVisiblePages = 5;
 
   const handlePageClick = (page) => {
     setCurrentPage(page);
@@ -11,18 +12,38 @@ const PageNation = ({ total, pageSize, setCurrentPage, currentPage }) => {
     currentPage < totalPageNum ? setCurrentPage(currentPage + 1) : alert("마지막 페이지 입니다.");
   };
   const handleArrowPrevClick = () => {
-    currentPage > 1 ? setCurrentPage(currentPage - 1) : alert("이전 페이지가 없습니다.");
+    currentPage > 1 ? setCurrentPage(currentPage - 1) : alert("처음 페이지 입니다.");
   };
+
+  const getPageNumbers = () => {
+    let startPage = Math.max(1, currentPage - Math.floor(maxVisiblePages / 2));
+    let endPage = startPage + maxVisiblePages - 1;
+
+    if (endPage > totalPageNum) {
+      endPage = totalPageNum;
+      startPage = Math.max(1, endPage - maxVisiblePages + 1);
+    }
+
+    const pages = [];
+    for (let i = startPage; i <= endPage; i++) {
+      pages.push(i);
+    }
+
+    return pages;
+  };
+
+  const pages = getPageNumbers();
+
   return (
     <ul className='pagination'>
       <li className='pagination-item arrow-prev' onClick={handleArrowPrevClick}></li>
-      {Array.from({ length: totalPageNum }, (_, i) => (
+      {pages.map((page) => (
         <li
-          key={i + 1}
-          onClick={() => handlePageClick(i + 1)}
-          className={`pagination-item ${i + 1 === currentPage ? "active" : ""}`}
+          key={page}
+          onClick={() => handlePageClick(page)}
+          className={`pagination-item ${page === currentPage ? "active" : ""}`}
         >
-          {i + 1}
+          {page}
         </li>
       ))}
       <li className='pagination-item arrow-next' onClick={handleArrowNextClick}></li>
