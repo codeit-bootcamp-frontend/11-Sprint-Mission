3d3bd782863d5207f5a2ebdcea31e03473c6a94e
양정화 <siltarre@gmail.com>
2024-10-05 05:20:17 +0900

    페이지 진입 시 로딩 보여주기

--- src/index.js
+++ src/index.js
@@ -1,11 +1,37 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
+import "./styles/loading.css";
 
+const RootComponent = () => {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const initializeApp = async () => {
+      try {
+        await new Promise((resolve) => setTimeout(resolve, 1000));
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    initializeApp();
+  }, []);
+
+  if (loading) {
+    return (
+      <div className='loading-spinner'>
+        <div className='loading-spinner-icon'></div>
+      </div>
+    );
+  }
+
+  return (
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  );
+};
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
-);
+root.render(<RootComponent />);
