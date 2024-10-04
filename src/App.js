49f4bc6be991ad12d21a2f6a33877bd63c5a1abc
SeokJiwoo <jiwoo9665@gmail.com>
2024-10-04 16:39:44 +0900

    ✨ Feat: 페이지네이션 추가

--- src/App.js
+++ src/App.js
@@ -4,10 +4,14 @@ import Header from './components/Header';
 import { useEffect, useState } from 'react';
 import { getProducts } from './hooks/api';
 
+const LIMIT = 6;
+
 function App() {
   const [order, setOrder] = useState('createAt');
   const [items, setItems] = useState([]);
   const [bestItem, setBestItem] = useState([]);
+  const [offset, setOffset] = useState(0);
+  // 더이상불러올데이터가없을때는어떻게구현해야될지..?(강의: 리액트데이터다루기-데이터더불러오기)
 
   const sortedItems = [...items].sort((a, b) => b[order] - a[order]);
   const BestItemsDesktop = bestItem.slice(0, 4);
@@ -15,14 +19,23 @@ function App() {
   const handleNewestClick = () => setOrder('createAt');
   const handleBestClick = () => setOrder('favoriteCount');
 
-  const handleLoad = async (orderQuery) => {
-    const { list } = await getProducts(orderQuery);
+  const handleLoad = async (options) => {
+    const { list } = await getProducts(options);
+    if (options.offset === 0) {
+      setItems(list);
+    } else {
+      setItems([...items, ...list]);
+    }
+    setOffset(options.offset + list.length);
     setBestItem(list);
-    setItems(list);
+  };
+
+  const handleLoadMore = () => {
+    handleLoad({ order, offset, limit: LIMIT });
   };
 
   useEffect(() => {
-    handleLoad(order);
+    handleLoad({ order, offset: 0, limit: LIMIT });
   }, [order]);
 
   return (
@@ -34,6 +47,7 @@ function App() {
       <button onClick={handleNewestClick}>최신순</button>
       <button onClick={handleBestClick}>베스트순</button>
       <ProductList items={sortedItems} />
+      <button onClick={handleLoadMore}>더보기</button>
     </div>
   );
 }
