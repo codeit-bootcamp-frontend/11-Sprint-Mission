5977134cf75cc64038c7e47e6b97120c866ea2cc
양정화 <siltarre@gmail.com>
2024-10-04 18:33:38 +0900

    수정 : productPrice 단위 별 콤마

--- src/components/Product/ProductPrice.js
+++ src/components/Product/ProductPrice.js
@@ -1,5 +1,6 @@
 const ProductPrice = ({ price }) => {
-  return <p className='product-price'>{price}원</p>;
+  const priceReplace = price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+  return <p className='product-price'>{priceReplace}원</p>;
 };
 
 export default ProductPrice;
