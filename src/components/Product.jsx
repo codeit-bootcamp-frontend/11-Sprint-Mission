b1a003a444bf2afc45d63f9bcc1adb791a1cee96
ksm <hpk5802@github.com>
2024-10-04 19:06:13 +0900

    refactor: ProductList, Product 리팩토링

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -5,15 +5,23 @@ import formatPriceToKRW from "utils/formatPrice";
 function Product({ product }) {
   const { id, name, price, favoriteCount, images, description } = product;
   const formattedPrice = formatPriceToKRW(price);
-  const image = images[0];
+  const image = images?.[0] || "/images/icons/ic_no_image.svg"; // 등록된 이미지가 없는 경우 대체 이미지(ic_no_image) 노출
+
+  /**
+   * // 이미지 로드에 에러 발생하는 경우 대체 이미지(ic_no_image) 노출
+   * @param {*} target img 태그
+   */
+  const handleImageError = (target) => {
+    target.src = "/images/icons/ic_no_image.svg";
+  };
   return (
     <Link to={`/items/${id}`} title={`${name} 상세보기`}>
       <div className="product">
         <div className="img-wrap">
           <img
-            src={image ? image : "/images/icons/ic_no_image.svg"} // 등록된 이미지가 없는 경우 대체 이미지(ic_no_image) 노출
-            alt={description}
-            onError={(e) => (e.target.src = "/images/icons/ic_no_image.svg")} // 이미지 로드에 에러 발생하는 경우 대체 이미지(ic_no_image) 노출
+            src={image}
+            alt={description || name}
+            onError={({ target }) => handleImageError(target)}
           />
         </div>
         <div className="desc-wrap">
--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -10,16 +10,18 @@ import Paginations from "./Paginations";
 
 function ProductList() {
   const navigate = useNavigate();
+  const initialDevice = getBreakpoint(window.innerWidth);
+  const initialperPage = updateProductsPerPage(initialDevice);
   const [products, setProducts] = useState([]); // 서버에서 받아올 Proudcts를 할당할 state
-  const [productsPerPage, setProductsPerPage] = useState(10); // 반응형에 따라 보여줄 Product 수를 할당할 state
-  const [device, setDevice] = useState("pc"); // 반응형 ('pc' | 'tablet' | 'mobile')
+  const [productsPerPage, setProductsPerPage] = useState(initialperPage); // 반응형에 따라 보여줄 Product 수를 할당할 state
+  const [device, setDevice] = useState(initialDevice); // 반응형 ('pc' | 'tablet' | 'mobile')
   const [order, setOrder] = useState("recent"); // 데이터 정렬을 위한 queryParam [orderBy]
   const [totalPage, setTotalPage] = useState(0); // Pagination을 위한 전체 페이지 수를 할당할 state
   const [currentPage, setCurrentPage] = useState(1); // 데이터 호출을 위한 queryParam [page]
 
   /**
    * pagination을 위한 totalPage 계산
-   * 재사용을 위해 useCallback Hook으로 함수 캐싱
+   * 재사용을 위해 useCallback Hook으로 함수 캐싱 -> React: exhustive-deps Eslint 방지
    * @param {*} totalCount 전체 Product 수
    * @param {*} perPage 화면에 보여질 Product 수 (반응형에 사용)
    */
@@ -27,20 +29,32 @@ function ProductList() {
     (totalCount, perPage) => {
       const totalPageCount = Math.ceil(totalCount / perPage);
       setTotalPage(totalPageCount);
-      if (currentPage > totalPageCount) {
-        setCurrentPage(totalPageCount);
-      }
+      if (currentPage > totalPageCount) setCurrentPage(totalPageCount);
     },
     [currentPage]
+  );
+
+  /**
+   * 서버에서 데이터를 가져오는 함수
+   * 재사용을 위해 useCallback Hook으로 함수 캐싱 -> React: exhustive-deps Eslint 방지
+   */
+  const loadProducts = useCallback(
+    (page = currentPage, pageSize = productsPerPage) => {
+      fetchProducts({ page, pageSize, orderBy: order }).then(
+        ({ list, totalCount }) => {
+          setProducts(list);
+          countTotalPage(totalCount, pageSize);
+        }
+      );
+    },
+    [currentPage, productsPerPage, order, countTotalPage]
   );
 
   /**
    * pagination 핸들러
    * @param {*} page active 페이지
    */
-  const handlePageChange = (page) => {
-    setCurrentPage(page);
-  };
+  const handlePageChange = (page) => setCurrentPage(page);
 
   useEffect(() => {
     /**
@@ -54,35 +68,16 @@ function ProductList() {
         const perPage = updateProductsPerPage(currentDevice);
         setDevice(currentDevice);
         setProductsPerPage(perPage);
-
-        fetchProducts({
-          page: currentPage,
-          pageSize: perPage,
-          orderBy: order,
-        }).then(({ list, totalCount }) => {
-          setProducts(list);
-          countTotalPage(totalCount, perPage);
-        });
+        // loadProducts(productsPerPage);
       }
     };
 
-    const initialDevice = getBreakpoint(window.innerWidth);
-    const initialPerPage = updateProductsPerPage(initialDevice);
-    setDevice(initialDevice);
-    setProductsPerPage(initialPerPage);
-
-    fetchProducts({
-      page: currentPage,
-      pageSize: initialPerPage,
-      orderBy: order,
-    }).then(({ list, totalCount }) => {
-      setProducts(list);
-      countTotalPage(totalCount, initialPerPage);
-    });
+    // initial 데이터 로드
+    loadProducts();
 
     window.addEventListener("resize", handleResize);
     return () => window.removeEventListener("resize", handleResize); // 언마운트: 리사이즈 이벤트 삭제
-  }, [device, order, currentPage, countTotalPage]);
+  }, [device, order, currentPage, countTotalPage, loadProducts]); // lint 경고
 
   return (
     <section id="section_all">
