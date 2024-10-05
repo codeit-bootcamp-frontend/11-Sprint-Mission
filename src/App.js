bbbcf21a20e896178392176edeffbbd4c16ec23b
WooLegend <dnwogus4260@naver.com>
2024-10-05 23:04:11 +0900

    feat: react-route-dom으로 경로 설정

--- src/App.js
+++ src/App.js
@@ -1,19 +1,28 @@
+import "./App.css";
 import ItemList from "./assets/components/ItemList/ItemList";
 import BestItemList from "./assets/components/BestItemList/BestItemList";
-import "./App.css";
 import Navigation from "./assets/components/Navigation/Navigation";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <>
+    <BrowserRouter>
       <header>
         <Navigation />
       </header>
-      <main className="main-wrapper">
-        <BestItemList />
-        <ItemList />
-      </main>
-    </>
+      <Routes>
+        <Route path="/items" element={<Main />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+function Main() {
+  return (
+    <main className="main-wrapper">
+      <BestItemList />
+      <ItemList />
+    </main>
   );
 }
 
