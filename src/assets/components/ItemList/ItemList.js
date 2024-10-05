942c2e332086b7d3363a5a7b0d1c36e230fc20d4
WooLegend <dnwogus4260@naver.com>
2024-10-05 18:40:51 +0900

    feat: 셀렉터 요소를 이용한 상품 정렬 기능 추가

--- src/assets/components/ItemList/ItemList.js
+++ src/assets/components/ItemList/ItemList.js
@@ -6,32 +6,39 @@ import "./ItemList.css";
 function ItemList() {
   const [items, setItems] = useState([]);
   const [page, setPage] = useState(1);
+  const [pageSize, setPageSize] = useState(12);
   const [order, setOrder] = useState("recent");
 
   useEffect(() => {
     const fetchData = async () => {
-      const result = await getProducts(1, 12, order);
+      const result = await getProducts(page, pageSize, order);
       setItems(result.list);
     };
     fetchData();
-  }, []);
+  }, [page, pageSize, order]);
 
   return (
     <div className="ItemList">
-      <Header />
+      <Header setOrder={setOrder} />
       <Content items={items} />
     </div>
   );
 }
 
-function Header() {
+function Header({ setOrder }) {
+  const handleSelectChange = (e) => {
+    const opt = e.target.options;
+    const selected = opt[opt.selectedIndex].value;
+    setOrder(selected);
+  };
+
   return (
     <div className="ItemList-header">
       <h2 className="title">전체 상품</h2>
       <div className="utils">
         <input className="search" placeholder="검색할 상품을 입력해주세요" />
         <button className="btn-add">상품 등록하기</button>
-        <select>
+        <select className="select-order" onChange={handleSelectChange}>
           <option value={"recent"}>최신순</option>
           <option value={"favorite"}>좋아요순</option>
         </select>
