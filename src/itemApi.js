11c0d57bb592d7fdf228e9dcf2596e73e8f309ce
1022gusl <1022gusl@gmail.com>
2024-10-05 16:18:58 +0900

    add itemAPI.js

---
+++ src/itemApi.js
@@ -0,0 +1,16 @@
+const ItemAPI_URL = "https://panda-market-api.vercel.app/products";
+
+export const fetchProducts = async () => {
+  try {
+    const response = await fetch(ItemAPI_URL);
+    if (!response.ok) {
+      throw new Error("Network response Error");
+    }
+
+    const body = await response.json();
+    return body;
+  } catch (error) {
+    console.error("Error fetching products: ", eeror);
+    throw error;
+  }
+};
