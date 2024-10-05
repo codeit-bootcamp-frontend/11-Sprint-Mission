246bda3c5fcf1df5ae2228ec31ad618624479daa
WooLegend <dnwogus4260@naver.com>
2024-10-05 17:31:43 +0900

    feat: 데스크탑 사이즈 반응형 전체 상품 리스트 개발

--- src/assets/components/ItemList/ItemList.js
+++ src/assets/components/ItemList/ItemList.js
@@ -1,30 +1,52 @@
 import { useEffect, useState } from "react";
 import { getProducts } from "../../../api";
 import Item from "../Item/Item";
+import "./ItemList.css";
 
 function ItemList() {
   const [items, setItems] = useState([]);
 
   useEffect(() => {
     const fetchData = async () => {
-      const result = await getProducts();
+      const result = await getProducts(1, 10);
       setItems(result.list);
     };
     fetchData();
   }, []);
 
-  console.log(items);
-
   return (
     <div className="ItemList">
-      <ul>
-        {items.map((item) => (
-          <li key={item.id}>
-            <Item item={item} />
-          </li>
-        ))}
-      </ul>
+      <Header />
+      <Content items={items} />
     </div>
+  );
+}
+
+function Header() {
+  return (
+    <div className="ItemList-header">
+      <h2 className="title">전체 상품</h2>
+      <div className="utils">
+        <input className="search" placeholder="검색할 상품을 입력해주세요" />
+        <button className="btn-add">상품 등록하기</button>
+        <select>
+          <option>최신순</option>
+          <option>좋아요순</option>
+        </select>
+      </div>
+    </div>
+  );
+}
+
+function Content({ items }) {
+  return (
+    <ul className="ItemList-content">
+      {items.map((item) => (
+        <li key={item.id}>
+          <Item item={item} />
+        </li>
+      ))}
+    </ul>
   );
 }
 
