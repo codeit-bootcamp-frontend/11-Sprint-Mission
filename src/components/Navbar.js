1cc7dce6d66a37cc68f8da9b2fbaa15d46bc0c97
bhoh032019 <bhoh0320@ajou.ac.kr>
2024-10-05 22:40:40 +0900

    feat: /items에 위치할 때 navbar 중고마켓 색상 변경

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,19 +1,27 @@
+import { useLocation } from 'react-router-dom';
 import '../style/Navbar.css';
 
 const Navbar = () => {
+  const location = useLocation();
+
   return (
     <nav className="navbar">
       <div className="nav-front">
         <a href="./index.html">
           <img
-            class="logo-image"
+            className="logo-image"
             src={process.env.PUBLIC_URL + '/images/Property 1=md.svg'}
             alt="판다마켓 로고"
           />
         </a>
         <div className="nav-link">
           <a href="#">자유게시판</a>
-          <a href="/items">중고마켓</a>
+          <a
+            href="/items"
+            className={location.pathname === '/items' ? 'active-link' : ''}
+          >
+            중고마켓
+          </a>
         </div>
       </div>
       <a className="login" href="signin.html">
