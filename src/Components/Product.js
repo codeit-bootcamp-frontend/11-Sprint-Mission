a69066aa26c315e03193847d6bb55f4bd948ef07
haksoo <haksoo0918@gmail.com>
2024-10-05 20:24:40 +0900

    추가: 이미지가 없거나 에러일때 처리

--- src/Components/Product.js
+++ src/Components/Product.js
@@ -1,10 +1,15 @@
 import iconHeart from '../assets/icon-heart.svg';
+import noImage from '../assets/no-image.svg';
 
-export default function Product({ image, name, price = 0, favoriteCount = 0 }) {
+export default function Product({ image = noImage, name, price = 0, favoriteCount = 0 }) {
+  const handleImageError = (e) => {
+    e.target.src = noImage;
+  };
+
   return (
     <div className="flex flex-col gap-2 flex-1">
       <figure className="aspect-square rounded-2xl overflow-hidden">
-        <img className="w-full h-full object-cover" src={image} alt={name} />
+        <img className="w-full h-full object-cover" src={image} alt={name} onError={handleImageError} />
       </figure>
       <h3 className="text-sm font-medium">{name}</h3>
       <div className="font-bold">{price}</div>
