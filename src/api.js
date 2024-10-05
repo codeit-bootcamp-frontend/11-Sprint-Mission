fdaa89ff32d02e8bf9dab3602753cb371e00fa61
WooLegend <dnwogus4260@naver.com>
2024-10-05 17:29:57 +0900

    feat: 상품 리스트 요청 API 파라미터 추가

--- src/api.js
+++ src/api.js
@@ -1,5 +1,28 @@
-export async function getProducts() {
-  const res = await fetch("https://panda-market-api.vercel.app/products");
+const originUrl = "https://panda-market-api.vercel.app";
+
+/**
+ * 상품 검색 API
+ * @param {Number} page 페이지 번호 (기본 : 1)
+ * @param {Number} pageSize 페이지당 상품 수 (기본 : 10)
+ * @param {Boolean} favorite false : 최신순(기본) true : 인기순
+ * @param {String} keyword 검색 키워드
+ * @returns 상품 리스트 객체 반환
+ */
+export async function getProducts(
+  page = 1,
+  pageSize = 10,
+  favorite = false,
+  keyword = undefined
+) {
+  const url = new URL(originUrl + "/products");
+  url.searchParams.append("page", page);
+  url.searchParams.append("pageSize", pageSize);
+  url.searchParams.append("orderBy", favorite ? "favorite" : "recent");
+  keyword && url.searchParams.append("keyword", keyword);
+
+  console.log(url.href);
+
+  const res = await fetch(url.href);
   const data = await res.json();
   return data;
 }
