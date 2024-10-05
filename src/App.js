803fd045d04aa8dbc849838d16bff5d56c7e216f
WooLegend <dnwogus4260@naver.com>
2024-10-05 22:12:19 +0900

    feat: 네비게이션 컴포넌트 개발
    개발 기능
    - 로고 아이콘
    - 네비게이션 탭
      현재 페이지에 해당하는 탭은 푸른색으로 강조된다.
      - 자유게시판 | path : /board
      - 중고마켓 | path : /items
    - 프로필 아이콘

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import ItemList from "./assets/components/ItemList/ItemList";
 import BestItemList from "./assets/components/BestItemList/BestItemList";
 import "./App.css";
+import Navigation from "./assets/components/Navigation/Navigation";
 
 function App() {
   return (
     <>
+      <header>
+        <Navigation />
+      </header>
       <main className="main-wrapper">
         <BestItemList />
         <ItemList />
