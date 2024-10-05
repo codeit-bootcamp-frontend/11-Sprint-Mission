ef7464d52edc834f70cb5dae762fee7bd5e09d83
WooLegend <dnwogus4260@naver.com>
2024-10-05 17:50:26 +0900

    feat: 베스트 상품 리스트, 전체 상품 리스트 배치

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
+import ItemList from "./assets/components/ItemList/ItemList";
+import BestItemList from "./assets/components/BestItemList/BestItemList";
+import "./App.css";
+
 function App() {
-  return <></>;
+  return (
+    <>
+      <main className="main-wrapper">
+        <BestItemList />
+        <ItemList />
+      </main>
+    </>
+  );
 }
 
 export default App;
