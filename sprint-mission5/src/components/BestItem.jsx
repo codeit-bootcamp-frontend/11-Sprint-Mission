ab6561900145050a3e72001c240bad5641f71600
kimdohoon2 <88535144+kimdohoon2@users.noreply.github.com>
2024-10-06 16:15:00 +0900

    feat : 베스트 컨포넌트 추가

---
+++ sprint-mission5/src/components/BestItem.jsx
@@ -0,0 +1,11 @@
+import "./BestItem.css";
+const BestItem = () => {
+  return (
+    <div>
+      <div className="bitop">
+        <h1>베스트 상품</h1>
+      </div>
+    </div>
+  );
+};
+export default BestItem;
