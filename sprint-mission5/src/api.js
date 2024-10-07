b57e63fe4f024b4744d562d85a4ee091d1ef2ca5
kimdohoon2 <88535144+kimdohoon2@users.noreply.github.com>
2024-10-07 20:40:28 +0900

    feat : Error메시지추가

--- sprint-mission5/src/api.js
+++ sprint-mission5/src/api.js
@@ -2,6 +2,9 @@ export async function getBestProducts({ pageSize = 4 }) {
   const response = await fetch(
     `https://panda-market-api.vercel.app/products?page=1&pageSize=${pageSize}&orderBy=favorite`
   );
+  if (!response.ok) {
+    throw new Error("베스트 상품 데이터를 불러오는데 실패했습니다");
+  }
   const data = await response.json();
   return data;
 }
@@ -16,7 +19,7 @@ export async function getAllProducts({
     `https://panda-market-api.vercel.app/products?${query}`
   );
   if (!response.ok) {
-    throw new Error("데이터를 불러오는데 실패했습니다");
+    throw new Error("전체 상품 데이터를 불러오는데 실패했습니다");
   }
   const data = await response.json();
   return data;
