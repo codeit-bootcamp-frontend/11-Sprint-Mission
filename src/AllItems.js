ecdf8d44a2ff43e190d67ad7ab5fcea9f15d2c68
김예도 <kyd629@naver.com>
2024-10-07 17:47:21 +0900

    전체 상품 구현

---
+++ src/AllItems.js
@@ -0,0 +1,76 @@
+import { useEffect, useState } from "react";
+import { getItems } from "./api";
+import "./css/AllItems.css";
+import searchIcon from "./img/icon/ic_search.png";
+import Item from "./Item.js";
+
+function BestItem() {
+  const [items, setItems] = useState([]);
+  const [orderBy, setOrderBy] = useState("recent");
+  const [keyword, setKeyword] = useState("");
+
+  const handleLoad = async (options) => {
+    let result;
+    try {
+      result = await getItems(options);
+    } catch (error) {
+      console.log(error);
+    }
+    setItems(result.list);
+  };
+
+  const handleKeyworSubmit = (e) => {
+    e.preventDefault();
+    setKeyword(e.target["keyword"].value);
+  };
+
+  const handleOrderByChange = (e) => {
+    setOrderBy(e.target.value);
+  };
+
+  useEffect(() => {
+    handleLoad({ orderBy, keyword });
+  }, [orderBy, keyword]);
+
+  return (
+    <div className="allItems">
+      <div className="allTitle">
+        <p>전체 상품</p>
+        <div className="allBars">
+          <div className="inputContainer">
+            <img className="searchIcon" src={searchIcon} />
+            <form onSubmit={handleKeyworSubmit}>
+              <input
+                name="keyword"
+                className="searchBar"
+                placeholder="검색할 상품을 입력해주세요"
+              />
+            </form>
+          </div>
+          <button className="registerItem">상품 등록하기</button>
+          <select className="selectSort" onChange={handleOrderByChange}>
+            <option value="recent">최신순</option>
+            <option value="favorite">좋아요 순</option>
+          </select>
+        </div>
+      </div>
+      <div className="indivItems">
+        {items && items.length > 0 ? (
+          items.map(({ id, images, name, price, favoriteCount }) => (
+            <Item
+              key={id}
+              image={images[0]}
+              name={name}
+              price={price}
+              favoriteCount={favoriteCount}
+            />
+          ))
+        ) : (
+          <p>상품을 찾는 중입니다..</p>
+        )}
+      </div>
+    </div>
+  );
+}
+
+export default BestItem;
--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import BestItem from "./BestItem";
 import Navbar from "./Navbar";
+import AllItems from "./AllItems";
 
 function App() {
   return (
     <div>
       <Navbar />
       <BestItem />
+      <AllItems />
     </div>
   );
 }
