df5704a26d35687c166cf8b81ead0cfb49bc3834
kimdohoon2 <88535144+kimdohoon2@users.noreply.github.com>
2024-10-07 18:06:55 +0900

    feat : 베스트 상품 불러오기

--- sprint-mission5/src/components/BestItem.jsx
+++ sprint-mission5/src/components/BestItem.jsx
@@ -1,9 +1,25 @@
 import "./BestItem.css";
+import { getBestProducts } from "../api";
+import ListItem from "./ItemList";
+import { useEffect, useState } from "react";
+
 const BestItem = () => {
+  const [productList, setProductList] = useState([]);
+
+  const loadList = async (options) => {
+    const { list } = await getBestProducts(options);
+    setProductList(list);
+  };
+  useEffect(() => {
+    loadList({ pageSize: 4 });
+  }, []);
   return (
-    <div>
-      <div className="bitop">
-        <h1>베스트 상품</h1>
+    <div className="bitop">
+      <h1>베스트 상품</h1>
+      <div className="best-item-con">
+        {productList?.map((item) => (
+          <ListItem item={item} key={`best-item-${item.id}`} />
+        ))}
       </div>
     </div>
   );
