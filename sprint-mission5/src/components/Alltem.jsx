5aecba0ba09fed8c78920d26050c574a0923df6a
kimdohoon2 <88535144+kimdohoon2@users.noreply.github.com>
2024-10-07 20:49:45 +0900

    feat : 최신순, 좋아요순 클릭시 데이터변경
    - Dropdownbtn.jsx에서 Alltem.jsx로 useState위치변경
    - 정렬 추가

--- sprint-mission5/src/components/Alltem.jsx
+++ sprint-mission5/src/components/Alltem.jsx
@@ -2,16 +2,56 @@ import Searchbox from "./Searchbox";
 import ItemSubmit from "./ItemSubmit";
 import DropDownbtn from "./DropDownbtn";
 import "./Alltem.css";
+import { getAllProducts } from "../api";
+import ListItem from "./ItemList";
+import { useEffect, useState } from "react";
 const Alltem = () => {
+  const [productList, setProductList] = useState([]);
+  const [order, setOrder] = useState("recent");
+  const handleRcentClick = () => {
+    setOrder("recent");
+    setIsOpen(false);
+  };
+  const handleFavoriteClick = () => {
+    setOrder("favorite");
+    setIsOpen(false);
+  };
+  const loadList = async (options) => {
+    const { list } = await getAllProducts(options);
+    setProductList(list);
+  };
+
+  useEffect(() => {
+    loadList({ page: 1, pageSize: 10, orderBy: order });
+  }, [order]);
+
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleDropdown = () => {
+    setIsOpen(!isOpen);
+  };
+
+  const sortedItems = productList.sort((a, b) => b[order] - a[order]);
   return (
     <div>
       <div className="aitop">
         <h1>전체상품</h1>
-        <div className="altop-flex">
+        <div className="aitop-flex">
           <Searchbox />
           <ItemSubmit />
-          <DropDownbtn />
+          <DropDownbtn
+            order={order}
+            isOpen={isOpen}
+            toggleDropdown={toggleDropdown}
+            handleRcentClick={handleRcentClick}
+            handleFavoriteClick={handleFavoriteClick}
+          />
         </div>
+      </div>
+      <div className="allitem-con">
+        {sortedItems?.map((item) => (
+          <ListItem item={item} key={`all-item-${item.id}`} />
+        ))}
       </div>
     </div>
   );
