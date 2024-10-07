282b48bf054d685d11572a992bca9b1599255f76
김예도 <kyd629@naver.com>
2024-10-07 22:06:35 +0900

    모바일 환경 로고 이미지 변경

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,12 +1,33 @@
-import mainImg from "./img/logo/logo.png";
+import mainLogoImg from "./img/logo/logo.png";
 import userImg from "./img/icon/user.png";
+import smallLogoImg from "./img/logo/smallLogo.png";
 import "./css/Navbar.css";
+import { useEffect, useState } from "react";
 
 function Navbar() {
+  const [logoImg, setLogoImg] = useState(mainLogoImg);
+
+  const updateLogoImage = () => {
+    const width = window.innerWidth;
+    if (width <= 768) {
+      setLogoImg(smallLogoImg);
+    } else {
+      setLogoImg(mainLogoImg);
+    }
+  };
+
+  useEffect(() => {
+    updateLogoImage();
+    window.addEventListener("resize", updateLogoImage);
+    return () => {
+      window.removeEventListener("resize", updateLogoImage);
+    };
+  }, []);
+
   return (
     <div className="Navibar">
       <div className="logoAndMenu">
-        <img className="MainLogo" src={mainImg} alt="판다마켓" />
+        <img className="MainLogo" src={logoImg} alt="판다마켓" />
         <div className="menu">
           <p>자유게시판</p>
           <p>중고마켓</p>
