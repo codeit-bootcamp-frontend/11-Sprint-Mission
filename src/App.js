2d1dd6d6abc5631f5cc60d8c1dd864fbd8b9b0bc
Jeongbin1 <petitjb05@gamil.com>
2024-10-08 19:59:22 +0900

    라우터 대소문자 문제

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
-import Signup from "./pages/SignUp";
+import SignUp from "./pages/signup";
 import Items from "./pages/Items";
 
 function App() {
@@ -9,7 +9,7 @@ function App() {
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/login" element={<Login />} />
-      <Route path="/signup" element={<Signup />} />
+      <Route path="/signup" element={<SignUp />} />
       <Route path="/items" element={<Items />} />
     </Routes>
   );
