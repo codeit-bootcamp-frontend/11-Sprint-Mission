36b960850e74178cb69bdb5ea8a7abb50db239f8
SeokJiwoo <jiwoo9665@gmail.com>
2024-10-08 19:58:18 +0900

    ✨ Feat: 태그 기능 추가

--- src/components/additem/AddProductInput.js
+++ src/components/additem/AddProductInput.js
@@ -1,3 +1,5 @@
+import { useState } from 'react';
+
 function AddProductInput({
   productName,
   productContent,
@@ -8,6 +10,26 @@ function AddProductInput({
   onPriceChange,
   onTagChange,
 }) {
+  const [tags, setTags] = useState([]);
+
+  const handleTagSubmit = () => {
+    if (productTag.trim() !== '') {
+      setTags([...tags, productTag.trim()]);
+      onTagChange('');
+    }
+  };
+
+  const handleKeyPress = (e) => {
+    if (e.key === ' ' || e.key === ',' || e.key === 'Enter') {
+      e.preventDefault();
+      handleTagSubmit();
+    }
+  };
+
+  const handleTagDelete = (indexToDelete) => {
+    setTags(tags.filter((_, index) => index !== indexToDelete));
+  };
+
   return (
     <form>
       <p>상품명</p>
@@ -33,8 +55,22 @@ function AddProductInput({
       <input
         value={productTag}
         onChange={(e) => onTagChange(e.target.value)}
+        onKeyDown={handleKeyPress}
         placeholder="태그를 입력해주세요"
       />
+
+      <div>
+        <ul>
+          {tags.map((tag, index) => (
+            <li key={index}>
+              {tag}{' '}
+              <button type="button" onClick={() => handleTagDelete(index)}>
+                X
+              </button>
+            </li>
+          ))}
+        </ul>
+      </div>
     </form>
   );
 }
