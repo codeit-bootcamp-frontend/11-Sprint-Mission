0e846e43723c6efa61e5220a1fe0144419afde09
Hwichul Shin <shinhwiiron@gmail.com>
2024-10-09 13:38:05 +0900

    라우터 적용 및 React 확장자 변경

--- Sprint-5-Mission/src/Layouts/Header.jsx
+++ Sprint-5-Mission/src/Layouts/Header.jsx
@@ -33,7 +33,7 @@ export function SubHeader() {
           </li>
 
           <li>
-            <a href="">중고마켓</a>
+            <a href="/">중고마켓</a>
           </li>
         </ul>
 
---
+++ Sprint-5-Mission/src/pages/items/Items.jsx
@@ -0,0 +1,15 @@
+import BestProduct from "./BestProduct";
+import AllProduct from "./AllProduct";
+import { SubHeader } from "../../Layouts/Header";
+
+const Items = () => {
+  return (
+    <>
+      <SubHeader />
+      <BestProduct />
+      <AllProduct />
+    </>
+  );
+};
+
+export default Items;
