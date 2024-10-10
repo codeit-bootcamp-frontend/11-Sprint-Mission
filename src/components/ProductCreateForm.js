86f3fae6e2fe2322d3b996177015cf221623726b
bhoh032019 <bhoh0320@ajou.ac.kr>
2024-10-10 18:31:35 +0900

    feat: additem페이지 기본 css적용

--- src/components/ProductCreateForm.js
+++ src/components/ProductCreateForm.js
@@ -4,27 +4,35 @@ import FileInput from './FileInput';
 function ProductCreateForm() {
   return (
     <div className="ProductContainer">
-      <div className="container-title">상품 등록하기</div>
-      <div className="ProductImg section-title">
-        상품 이미지
-        <FileInput name="imgFile" />
+      <div className="container-title">
+        상품 등록하기
+        <button className="Registerbtn">등록</button>
       </div>
-      <div className="ProductTitle section-title">
-        상품명
-        <input name="title" placeholder="상품명을 입력해주세요"></input>
+      <div className="container-body">
+        <div className="ProductImg section-title">
+          상품 이미지
+          <FileInput name="imgFile" />
+        </div>
+        <div className="ProductTitle section-title">
+          상품명
+          <input name="title" placeholder="상품명을 입력해주세요"></input>
+        </div>
+        <div className="ProductIntro section-title">
+          상품소개
+          <textarea
+            name="content"
+            rows={5}
+            placeholder="상품 소개를 입력해 주세요"
+          ></textarea>
+        </div>
+        <div className="ProductPrice section-title">
+          판매가격
+          <input name="price" placeholder="판매 가격을 입력해주세요"></input>
+        </div>
+        <div className="ProductTag section-title">
+          태그<input name="tag" placeholder="태그를 입력해주세요"></input>
+        </div>
       </div>
-      <div className="ProductIntro section-title">
-        상품소개
-        <textarea
-          name="content"
-          placeholder="상품 소개를 입력해 주세요"
-        ></textarea>
-      </div>
-      <div className="ProductPrice section-title">
-        판매가격
-        <input name="price"></input>
-      </div>
-      <div className="section-title">태그</div>
     </div>
   );
 }
