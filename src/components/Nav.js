1de63c7d31b1a691c9080e126658d6d5e4812ce4
JunHee <z1top159@gmail.com>
2024-10-10 16:07:19 +0900

    feat: 이미지 링크 추가

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -2,6 +2,7 @@ import { useLocation } from "react-router-dom";
 import logoLarge from "../assets/Property 1=lg.png";
 import profileIcon from "../assets/Frame 2609463.png";
 import "./common.css";
+import "./Nav.css";
 
 const Nav = () => {
   const location = useLocation();
@@ -10,17 +11,26 @@ const Nav = () => {
     <nav className="nav-container">
       <div className="nav-bar">
         <div className="nav-front">
-          <img className="logoLarge" src={logoLarge} alt="판다마켓 로고" />
+          <a href="/index">
+            <img className="nav-logo" src={logoLarge} alt="판다마켓 로고" />
+          </a>
           <div className="nav-board">
-            <a href="#" className="nav-board-free-board">
+            <a href="/freeBoard" className="nav-board-free-board">
               자유게시판
             </a>
-            <a href="/items" className="nav-board-used-market">
+            <a
+              href="/items"
+              className={`nav-board-used-market ${
+                location.pathname === "/items" ? "active" : ""
+              }`}
+            >
               중고마켓
             </a>
           </div>
         </div>
-        <img className="nav-end" src={profileIcon} alt="프로필 아이콘" />
+        <a href="/profile">
+          <img className="nav-icon" src={profileIcon} alt="프로필 아이콘" />
+        </a>
       </div>
     </nav>
   );
