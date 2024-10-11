7150d9544b19014a4d838b2efe4fab64077dff91
홍지윤 <h_zhirun@naver.com>
2024-10-11 14:16:04 +0900

    Nav 구현 및 Router 설정

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Freeboard from './pages/Freeboard';
+import Items from './pages/Items';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/freeboard" element={<Freeboard />} />
+        <Route path="/items" element={<Items />} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/pages/Items.js
@@ -0,0 +1,12 @@
+// src/pages/Items.js
+import React from 'react';
+
+function Items() {
+  return (
+    <div>
+      <h1>중고마켓 페이지입니다.</h1>
+    </div>
+  );
+}
+
+export default Items;
