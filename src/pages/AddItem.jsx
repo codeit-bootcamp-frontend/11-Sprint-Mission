fe3514842b0b6d167be301a231cda3af6d0c6a1e
양정화 <siltarre@gmail.com>
2024-10-11 21:14:03 +0900

    addItem page 스타일정리

--- src/pages/AddItem.jsx
+++ src/pages/AddItem.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import styles from "./AddItem.module.scss";
 import HeadingTitleArea from "../components/common/HeadingTitleArea";
 import Button from "../components/common/Button";
 import Input from "../components/common/Input";
@@ -6,51 +7,70 @@ import InputFile from "../components/AddItem/InputFile";
 
 function AddItem() {
   return (
-    <main className='page-addItem'>
-      <div className='container'>
-        <HeadingTitleArea>
-          <h2>상품 등록하기</h2>
-          <Button
-            link={false}
-            className='post'
-            styleType='square gray small_40'
-          >
-            등록
-          </Button>
-        </HeadingTitleArea>
-        <HeadingTitleArea>
-          <h3>상품이미지</h3>
-        </HeadingTitleArea>
-        <InputFile />
-        <HeadingTitleArea>
-          <h3>상품명</h3>
-        </HeadingTitleArea>
-        <Input placeholder='상품명을 입력해주세요' inputName='productName' />
-        <HeadingTitleArea>
-          <h3>상품소개</h3>
-        </HeadingTitleArea>
-        <Input
-          isInput={false}
-          placeholder='상품 소개를 입력해주세요'
-          name='productDescription'
-        />
-        <HeadingTitleArea>
-          <h3>판매가격</h3>
-        </HeadingTitleArea>
-        <Input
-          placeholder='판매 가격을 입력해주세요'
-          inputName='productPrice'
-        />
-        <HeadingTitleArea>
-          <h3>태그</h3>
-        </HeadingTitleArea>
-        <div className='input-tag-area'>
-          <Input placeholder='태그를 입력해주세요' inputName='productTag' />
-          <ul className='input-tag-list'>
-            <li className='input-tag-item'>#티셔츠</li>
-            <li className='input-tag-item'>#상의</li>
-          </ul>
-        </div>
+    <main className="page-addItem">
+      <div className="container">
+        <form>
+          <HeadingTitleArea>
+            <h2>상품 등록하기</h2>
+            <Button
+              link={false}
+              className="post"
+              styleType="square gray small_40">
+              등록
+            </Button>
+          </HeadingTitleArea>
+          <div className={styles.inputTextArea}>
+            <div>
+              <HeadingTitleArea>
+                <h3>상품이미지</h3>
+              </HeadingTitleArea>
+              <InputFile />
+            </div>
+            <div>
+              <HeadingTitleArea>
+                <h3>상품명</h3>
+              </HeadingTitleArea>
+              <Input
+                placeholder="상품명을 입력해주세요"
+                inputName="productName"
+              />
+            </div>
+            <div>
+              <HeadingTitleArea>
+                <h3>상품소개</h3>
+              </HeadingTitleArea>
+              <Input
+                isInput={false}
+                placeholder="상품 소개를 입력해주세요"
+                name="productDescription"
+              />
+            </div>
+            <div>
+              <HeadingTitleArea>
+                <h3>판매가격</h3>
+              </HeadingTitleArea>
+              <Input
+                placeholder="판매 가격을 입력해주세요"
+                inputName="productPrice"
+              />
+            </div>
+            <div>
+              <HeadingTitleArea>
+                <h3>태그</h3>
+              </HeadingTitleArea>
+              <div className="input-tag-area">
+                <Input
+                  placeholder="태그를 입력해주세요"
+                  inputName="productTag"
+                />
+                <ul className="input-tag-list">
+                  <li className="input-tag-item">#티셔츠</li>
+                  <li className="input-tag-item">#상의</li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </form>
       </div>
     </main>
   );
