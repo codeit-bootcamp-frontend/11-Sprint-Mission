60bb62a636b9bc86ddab7afb5fa1625bc03936e6
ksm <hpk5802@github.com>
2024-10-11 16:13:37 +0900

    feat: 중복, 공백 태그 추가 방지 로직 추가

--- src/components/addItem/TagInput.jsx
+++ src/components/addItem/TagInput.jsx
@@ -1,12 +1,19 @@
 import React, { useEffect, useState } from "react";
 
 function TagInput({ children, name, placeholder, setUserInput }) {
-  const [value, setValue] = useState("");
-  const [tags, setTags] = useState([]);
+  const [value, setValue] = useState(""); // input state
+  const [tags, setTags] = useState([]); // 화면 노출 및 form의 tags를 위한 state
+
+  /**
+   * 엔터를 입력 받으면 태그 추가 - 공백, 중복 추가 불가
+   * @param {*} e keydownEvent
+   */
   const handleKeyDown = (e) => {
     if (e.key === "Enter") {
       e.preventDefault();
-      if (e.target.value.trim() !== "") {
+      const isValidAddTag =
+        e.target.value.trim() !== "" && tags.indexOf(e.target.value); // 공백 & 중복 태그 확인
+      if (isValidAddTag) {
         setTags((prev) => [...prev, e.target.value]);
         setValue("");
       }
@@ -17,8 +24,16 @@ function TagInput({ children, name, placeholder, setUserInput }) {
     setValue(e.target.value);
   };
 
+  /**
+   * x버튼 클릭 시 태그 삭제
+   * @param {*} index 태그 삭제를 위한 인덱싱 변수
+   * @returns
+   */
   const deleteTag = (index) => setTags(tags.filter((_, idx) => idx !== index));
 
+  /**
+   * tag state가 변할 때(추가, 삭제) form의 tags 업데이트
+   */
   useEffect(() => {
     setUserInput((prev) => ({
       ...prev,
