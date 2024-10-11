e8b3e828f65c36e1a9478990f9f35badb8ce412d
ksm <hpk5802@github.com>
2024-10-11 17:02:04 +0900

    comment: 주석 추가

--- src/components/addItem/PriceInput.jsx
+++ src/components/addItem/PriceInput.jsx
@@ -2,11 +2,14 @@ import React from "react";
 import { formatPrice, formatToPrice } from "utils/formatPrice";
 
 function PriceInput({ children, name, value, setUserInput }) {
+  /**
+   * String 타입의 입력을 금액 형식으로 formating해 input에 출력
+   */
   const handleInput = ({ target }) => {
     const priceValue = formatToPrice(target.value);
-    const regExp = /^\d*$/;
+    const regExp = /^\d*$/; // 숫자만 입력 가능하게 하기 위한 정규식
     if (regExp.test(priceValue)) {
-      setUserInput((prev) => ({ ...prev, price: +priceValue }));
+      setUserInput((prev) => ({ ...prev, price: +priceValue })); // form의 price state 저장할 때는 Number 타입으로 변환해서 저장
     }
   };
   return (
