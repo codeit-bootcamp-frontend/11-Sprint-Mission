023cca6ad17e6cf800b8371e4af76a5d99ebc35e
양정화 <siltarre@gmail.com>
2024-10-12 01:42:58 +0900

    초기값을 innerWidth값에 따라 조건설정하여 리턴

--- src/hooks/useProductsPageSize.js
+++ src/hooks/useProductsPageSize.js
@@ -1,8 +1,19 @@
 import { useState, useEffect, useCallback } from "react";
 
 const useResponsivePageSize = ({ mobileSize, tabletSize, pcSize }) => {
-  const [pageSize, setPageSize] = useState(pcSize);
+  // 초기값을 계산하는 함수
+  const getInitialPageSize = () => {
+    if (window.innerWidth <= 767) {
+      return mobileSize;
+    } else if (window.innerWidth <= 1199) {
+      return tabletSize;
+    }
+    return pcSize;
+  };
 
+  const [pageSize, setPageSize] = useState(getInitialPageSize);
+
+  // 화면 크기가 변경될 때 마다 실행되는 함수
   const getListItemCountByScreenSize = useCallback(() => {
     if (window.matchMedia("(max-width: 767px)").matches) {
       return mobileSize;
