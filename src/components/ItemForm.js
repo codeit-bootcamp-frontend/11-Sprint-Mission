3b65822a1c5d09a4046d5b66f3f0fd3e94c5e123
김예도 <kyd629@naver.com>
2024-10-12 13:36:58 +0900

    등록 버튼 수정

--- src/components/ItemForm.js
+++ src/components/ItemForm.js
@@ -41,46 +41,54 @@ function ItemForm() {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <p>상품 이미지</p>
-      <FileInput
-        name="imgFile"
-        value={values.imgFile}
-        onChange={handleChange}
-      />
-      <p>상품명</p>
-      <input
-        className="input name"
-        name="name"
-        value={values.name}
-        onChange={handleInputChange}
-        placeholder="상품명을 입력해주세요"
-      />
-      <p>상품소개</p>
-      <textarea
-        className="input description"
-        name="description"
-        value={values.description}
-        onChange={handleInputChange}
-        placeholder="상품 소개를 입력해주세요"
-      />
-      <p>상품가격</p>
-      <input
-        className="input price"
-        name="price"
-        value={values.price}
-        onChange={handleInputChange}
-        placeholder="판매 가격을 입력해주세요"
-      />
-      <p>태그</p>
-      <input
-        className="input tags"
-        name="tags"
-        value={values.tags}
-        onChange={handleInputChange}
-        placeholder="태그를 입력해주세요"
-      />
-    </form>
+    <div>
+      <form onSubmit={handleSubmit}>
+        <div className="pageTitle">
+          <h2>상품 등록하기</h2>
+          <button type="submit" className="register">
+            등록
+          </button>
+        </div>
+        <p>상품 이미지</p>
+        <FileInput
+          name="imgFile"
+          value={values.imgFile}
+          onChange={handleChange}
+        />
+        <p>상품명</p>
+        <input
+          className="input name"
+          name="name"
+          value={values.name}
+          onChange={handleInputChange}
+          placeholder="상품명을 입력해주세요"
+        />
+        <p>상품소개</p>
+        <textarea
+          className="input description"
+          name="description"
+          value={values.description}
+          onChange={handleInputChange}
+          placeholder="상품 소개를 입력해주세요"
+        />
+        <p>상품가격</p>
+        <input
+          className="input price"
+          name="price"
+          value={values.price}
+          onChange={handleInputChange}
+          placeholder="판매 가격을 입력해주세요"
+        />
+        <p>태그</p>
+        <input
+          className="input tags"
+          name="tags"
+          value={values.tags}
+          onChange={handleInputChange}
+          placeholder="태그를 입력해주세요"
+        />
+      </form>
+    </div>
   );
 }
 
--- src/pages/additems.js
+++ src/pages/additems.js
@@ -4,10 +4,6 @@ import ItemForm from "../components/ItemForm.js";
 function AddItems() {
   return (
     <div className="addItemPage">
-      <div className="pageTitle">
-        <h2>상품 등록하기</h2>
-        <button className="register">등록</button>
-      </div>
       <form>
         <ItemForm />
       </form>
