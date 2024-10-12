16914ae2f7527fb75c60c4ac0c78a14930075bbb
haksoo <haksoo0918@gmail.com>
2024-10-12 20:11:42 +0900

    추가: 상품등록 페이지

---
+++ src/Components/FileInput.js
@@ -0,0 +1,93 @@
+import { useEffect, useRef, useState } from 'react';
+import styles from './FileInput.module.css';
+
+function FileInput({ name, value, onChange }) {
+  // 인풋 파일 요소
+  const inputRef = useRef();
+  // 미리보기
+  const [preview, setPreview] = useState(null);
+
+  // 파일 값 변경 처리
+  const handleChange = (e) => {
+    onChange(name, e.target.files[0]);
+  };
+  // 선택한 이미지 파일 삭제
+  const handleClearClick = () => {
+    const inputNode = inputRef.current;
+
+    if (inputNode) {
+      inputNode.value = '';
+      onChange(name, null);
+    }
+  };
+
+  useEffect(() => {
+    if (!value) return;
+
+    // 이미지 파일 미리보기 설정
+    const previewImageUrl = URL.createObjectURL(value);
+    setPreview(previewImageUrl);
+
+    // 정리함수
+    return () => {
+      setPreview(null);
+      URL.revokeObjectURL(previewImageUrl);
+    };
+  }, [value]);
+
+  return (
+    <div className="flex gap-6">
+      <label htmlFor="images" className={styles.insteadFile}>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 24 24"
+          fill="currentColor"
+          className="size-12">
+          <path
+            fillRule="evenodd"
+            d="M12 3.75a.75.75 0 0 1 .75.75v6.75h6.75a.75.75 0 0 1 0 1.5h-6.75v6.75a.75.75 0 0 1-1.5 0v-6.75H4.5a.75.75 0 0 1 0-1.5h6.75V4.5a.75.75 0 0 1 .75-.75Z"
+            clipRule="evenodd"
+          />
+        </svg>
+        이미지 등록
+      </label>
+      <input
+        type="file"
+        id="images"
+        name="images"
+        accept="image/png, image/jpeg"
+        ref={inputRef}
+        onChange={handleChange}
+      />
+      {preview && (
+        <figure className={styles.wrapPreview}>
+          <img src={preview} alt="상품 이미지 미리보기" />
+          <button className={styles.btnClose} type="button" onClick={handleClearClick}>
+            <svg
+              width="22"
+              height="24"
+              viewBox="0 0 22 24"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg">
+              <circle cx="11" cy="12" r="10" fill="#9CA3AF" />
+              <path
+                d="M7.08044 8L15.0804 16"
+                stroke="white"
+                stroke-width="1.8"
+                stroke-linecap="round"
+              />
+              <path
+                d="M14.9999 8L6.99994 16"
+                stroke="white"
+                stroke-width="1.8"
+                stroke-linecap="round"
+              />
+            </svg>
+          </button>
+        </figure>
+      )}
+    </div>
+  );
+}
+
+export default FileInput;
--- src/Pages/AddItem.js
+++ src/Pages/AddItem.js
@@ -1,13 +1,184 @@
-// switch gnb class
-// document.body.classList
-// find
-
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { switchGnbClass } from '../utils/switchGnbClass';
+import styles from './AddItem.module.css';
+import FileInput from '../Components/FileInput';
 
-export default function AddItem() {
+function Tag({ idx, children, onDelete }) {
+  // tag 삭제
+  const handleDeleteClick = () => {
+    onDelete(idx);
+  };
+
+  return (
+    <span className={styles.tag}>
+      #{children}
+      <svg
+        className="cursor-pointer"
+        onClick={handleDeleteClick}
+        width="22"
+        height="24"
+        viewBox="0 0 22 24"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg">
+        <circle cx="11" cy="12" r="10" fill="#9CA3AF" />
+        <path
+          d="M7.08032 8L15.0803 16"
+          stroke="#F9FAFB"
+          stroke-width="1.8"
+          stroke-linecap="round"
+        />
+        <path d="M15 8L7 16" stroke="#F9FAFB" stroke-width="1.8" stroke-linecap="round" />
+      </svg>
+    </span>
+  );
+}
+
+// form 초기값
+const INITIAL_VALUES = {
+  images: null,
+  name: '',
+  description: '',
+  price: 0,
+  tags: [],
+};
+
+function AddItem() {
+  const [values, setValues] = useState(INITIAL_VALUES);
+  const [btnDisabled, setBtnDisabled] = useState(true);
+
+  // 값 변경에 따른 처리: 비제어
+  const handleChange = (name, value) => {
+    setValues((prevValues) => ({
+      ...prevValues,
+      [name]: value,
+    }));
+  };
+
+  // 인풋 값 변경에 따른 처리: 제어
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    handleChange(name, value);
+  };
+
+  // tag 값 입력
+  const handleTagsKeydown = (e) => {
+    // console.log('fn:handleTagsKeydown:', e);
+    if (e.key !== 'Enter') return;
+    e.preventDefault();
+
+    const el = e.target;
+    const value = el.value.trim();
+    const hasValue = values.tags.some((tag) => tag === value);
+
+    // 빈값 & 중복 체크
+    if (value && !hasValue) {
+      handleChange('tags', [...values.tags, value]);
+    }
+    el.value = '';
+  };
+  // tag 값 삭제
+  const onTagDelete = (idx) => {
+    handleChange(
+      'tags',
+      values.tags.filter((item, index) => index !== idx)
+    );
+    console.log(idx);
+  };
+
+  // 폼 서브밋 처리
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const formData = new FormData();
+    for (const key in values) {
+      formData.append(key, values[key]);
+    }
+  };
+
   useEffect(() => {
+    // 상단 네비 '중고마켓' 활성화
     switchGnbClass('market');
   }, []);
-  return <h1>AddItem</h1>;
+
+  useEffect(() => {
+    // 버튼 활성화
+    const validValues = [values.name, values.description, values.price > 0, values.tags.length > 0];
+    const disabled = validValues.some((value) => Boolean(value) === false);
+    setBtnDisabled(disabled);
+  }, [values]);
+
+  return (
+    <form className="mt-6 mb-16 flex flex-col gap-8" onSubmit={handleSubmit}>
+      <div className="flex justify-between">
+        <h2 className={styles.title}>상품 등록하기</h2>
+        <button className="btn" type="submit" disabled={btnDisabled}>
+          등록
+        </button>
+      </div>
+
+      <div className={styles.formGroup}>
+        <label htmlFor="images">상품 이미지</label>
+        <FileInput name="images" value={values.images} onChange={handleChange} />
+      </div>
+
+      <div className={styles.formGroup}>
+        <label htmlFor="name">상품명</label>
+        <input
+          id="name"
+          name="name"
+          type="text"
+          value={values.name}
+          placeholder="상품명을 입력해주세요"
+          onChange={handleInputChange}
+          required
+        />
+      </div>
+
+      <div className={styles.formGroup}>
+        <label htmlFor="description">상품 소개</label>
+        <textarea
+          id="description"
+          name="description"
+          value={values.description}
+          placeholder="상품 소개를 입력해주세요"
+          rows="8"
+          onChange={handleInputChange}
+          required
+        />
+      </div>
+
+      <div className={styles.formGroup}>
+        <label htmlFor="price">판매 가격</label>
+        <input
+          id="price"
+          name="price"
+          value={values.price}
+          type="number"
+          placeholder="판매 가격을 입력해주세요"
+          onChange={handleInputChange}
+          required
+        />
+      </div>
+
+      <div className={styles.formGroup}>
+        <label htmlFor="tags">태그</label>
+        <input
+          id="tags"
+          type="text"
+          name="tags"
+          placeholder="태그를 입력해주세요"
+          onKeyDown={handleTagsKeydown}
+        />
+        <div className="flex gap-3">
+          {values.tags.map((tag, index) => (
+            <Tag idx={index} onDelete={onTagDelete} key={tag}>
+              {tag}
+            </Tag>
+          ))}
+        </div>
+      </div>
+    </form>
+  );
 }
+
+export default AddItem;
