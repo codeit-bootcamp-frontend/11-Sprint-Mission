9e943cb22c3aff24c7b00c9c009ab4890cf59ea6
김예도 <kyd629@naver.com>
2024-10-12 15:43:36 +0900

    이미지 삭제 로직 구현

--- src/components/FileInput.js
+++ src/components/FileInput.js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import "./FileInput.css";
 import UPLOADIMAGE from "../img/icon/upload.png";
+import DELETE from "../img/icon/ic_X.png";
 
 function FileInput({ name, value, onChange }) {
   const [preview, setPreview] = useState();
@@ -18,6 +19,13 @@ function FileInput({ name, value, onChange }) {
 
   const handleImageClick = () => {
     document.getElementById("fileInput").click();
+  };
+
+  const handleImageDelte = () => {
+    setPreview(null);
+    setImgError("");
+    const fileInput = document.getElementById("fileInput");
+    fileInput.value = null;
   };
 
   useEffect(() => {
@@ -43,7 +51,17 @@ function FileInput({ name, value, onChange }) {
           />
           <input type="file" id="fileInput" onChange={handleChange} />
         </div>
-        {preview && <img className="previewImg" src={preview} alt="미리보기" />}
+        {preview && (
+          <div className="previewContainer">
+            <img className="previewImg" src={preview} alt="미리보기" />
+            <img
+              className="ImgDeleteIcon"
+              src={DELETE}
+              alt="이미지 삭제"
+              onClick={handleImageDelte}
+            />
+          </div>
+        )}
       </div>
       {imgError && <p className="ImgErrorMsg">{imgError}</p>}
     </div>
