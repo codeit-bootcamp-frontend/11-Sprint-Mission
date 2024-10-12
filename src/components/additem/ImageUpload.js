356bbb5dec355860d6d4e33c575d0c498995ee18
SeokJiwoo <jiwoo9665@gmail.com>
2024-10-12 18:03:33 +0900

    ✨ Feat: 이미지 등록 에러메세지 추가

--- src/components/additem/ImageUpload.js
+++ src/components/additem/ImageUpload.js
@@ -2,6 +2,7 @@ import { useRef, useState, useEffect } from 'react';
 
 function ImageUpload({ name, value, onChange }) {
   const [preview, setPreview] = useState();
+  const [message, setMessage] = useState(''); // 메시지 상태 관리
   const inputRef = useRef(null);
 
   const handleChange = (e) => {
@@ -15,6 +16,7 @@ function ImageUpload({ name, value, onChange }) {
 
     inputNode.value = '';
     onChange(name, null);
+    setMessage('');
   };
 
   useEffect(() => {
@@ -29,36 +31,50 @@ function ImageUpload({ name, value, onChange }) {
     };
   }, [value]);
 
-  return (
-    <div className="imageUploadContainer">
-      <div className="customFileInput">
-        <label for="fileUpload" className="uploadBox">
-          <div className="uploadBoxContent">
-            <p className="textArea">
-              <span className="plus">+</span>
-              <br /> 이미지 등록
-            </p>
-          </div>
-        </label>
-        <input
-          id="fileUpload"
-          className="imageUploadInput"
-          type="file"
-          accept="image/png, image/jpeg"
-          ref={inputRef}
-          onChange={handleChange}
-          style={{ display: 'none' }}
-        />
-      </div>
+  const handleLabelClick = (e) => {
+    if (preview) {
+      e.preventDefault();
+      setMessage('*이미지 등록은 최대 1개까지 가능합니다.');
+    }
+  };
 
-      {preview && (
-        <div className="imagePreviewContainer">
-          <img src={preview} alt="미리보기 이미지" className="imagePreview" />
-          <button className="clearButton" onClick={handleClearClick}>
-            X
-          </button>
+  return (
+    <div>
+      <div className="imageUploadContainer">
+        <div className="customFileInput">
+          <label
+            htmlFor="fileUpload"
+            className="uploadBox"
+            onClick={handleLabelClick} // 라벨 클릭 이벤트 추가
+          >
+            <div className="uploadBoxContent">
+              <p className="textArea">
+                <span className="plus">+</span>
+                <br /> 이미지 등록
+              </p>
+            </div>
+          </label>
+          <input
+            id="fileUpload"
+            className="imageUploadInput"
+            type="file"
+            accept="image/png, image/jpeg"
+            ref={inputRef}
+            onChange={handleChange}
+            style={{ display: 'none' }}
+          />
         </div>
-      )}
+
+        {preview && (
+          <div className="imagePreviewContainer">
+            <img src={preview} alt="미리보기 이미지" className="imagePreview" />
+            <button className="clearButton" onClick={handleClearClick}>
+              X
+            </button>
+          </div>
+        )}
+      </div>
+      {message && <p className="errorMessage">{message}</p>}
     </div>
   );
 }
