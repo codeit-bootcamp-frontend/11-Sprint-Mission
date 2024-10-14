9fa3abffa2fba0687652e5b29e4535fbbf8fe3ad
JunHee <z1top159@gmail.com>
2024-10-14 11:05:38 +0900

    feat: axios 설치 및 적용

--- src/api.js
+++ src/api.js
@@ -1,11 +1,12 @@
-const BASE_URL = process.env.REACT_APP_BASE_URL;
+import instance from "./axiosInstance";
 
 export async function getProducts(params = {}) {
-  const query = new URLSearchParams(params).toString();
-  const response = await fetch(`${BASE_URL}/products?${query}`);
-  if (!response.ok) {
+  try {
+    const response = await instance.get("/products", {
+      params,
+    });
+    return response.data;
+  } catch (error) {
     throw new Error("정보를 불러오는데 실패했습니다.");
   }
-  const body = await response.json();
-  return body;
 }
