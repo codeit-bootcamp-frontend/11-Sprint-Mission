0ad9f84303ae45b5a19e8bd612ae17976a22fc5a
kimdohoon2 <88535144+kimdohoon2@users.noreply.github.com>
2024-10-14 22:32:10 +0900

    feat : 베스트 아이템 템플릿 모바일 갯수 다르게 보이기

--- sprint-mission5/src/components/BestItem.jsx
+++ sprint-mission5/src/components/BestItem.jsx
@@ -1,18 +1,41 @@
 import "./BestItem.css";
-import { getBestProducts } from "../api";
+import { getBestProducts } from "../api/api";
 import ListItem from "./ItemList";
 import { useEffect, useState } from "react";
 
+const getPageSize = () => {
+  const width = window.innerWidth;
+  if (width < 768) {
+    // Mobile viewport
+    return 1;
+  } else if (width < 1279) {
+    // Tablet viewport
+    return 2;
+  } else {
+    // Desktop viewport
+    return 4;
+  }
+};
 const BestItem = () => {
   const [productList, setProductList] = useState([]);
+  const [pageSize, setPageSize] = useState(getPageSize());
 
-  const loadList = async (options) => {
-    const { list } = await getBestProducts(options);
-    setProductList(list);
+  const loadList = async ({ orderBy, page, pageSize }) => {
+    const itmes = await getBestProducts({ orderBy, page, pageSize });
+    setProductList(itmes.list);
   };
   useEffect(() => {
-    loadList({ pageSize: 4 });
-  }, []);
+    const handleResize = () => {
+      setPageSize(getPageSize());
+    };
+
+    window.addEventListener("resize", handleResize);
+    loadList({ orderBy: "favorite", pageSize });
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, [pageSize]);
   return (
     <div className="bitop">
       <h1>베스트 상품</h1>
