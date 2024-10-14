e138586b2beaf93187e022f12820a812dd1a7212
lemon1335 <kimjinhui114@gmail.com>
2024-10-14 10:37:15 +0900

    style change x-button style

--- src/components/Tag.js
+++ src/components/Tag.js
@@ -1,11 +1,16 @@
 import "./Tag.css";
+import x from "../assets/ic_x.svg";
 
 function Tag({ children }) {
   return (
     <>
       <div className="single-tag">
-        {children}
-        <button className="x-button">x</button>
+        <div className="tag-content">
+          <p>{children}</p>
+          <button className="x-button">
+            <img src={x} alt="태그삭제" />
+          </button>
+        </div>
       </div>
     </>
   );
