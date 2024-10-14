f3d3ee089e18dba2de5ae16c4efe5782db21dfaf
JunHee <z1top159@gmail.com>
2024-10-14 10:45:32 +0900

    feat: Nav 중첩 레이아웃 적용

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import Items from "./pages/Items";
 import AddItem from "./pages/AddItem";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Nav from "./components/Nav";
+import { BrowserRouter, Route, Routes, Outlet } from "react-router-dom";
 
 function App() {
   return (
     <BrowserRouter>
+      <Nav />
       <Routes>
-        <Route path="/" element={<Items />} />
-        <Route path="/items" element={<Items />} />
-        <Route path="*" element={<div>페이지를 찾을 수 없습니다.</div>} />
-        <Route path="/additem" element={<AddItem />} />
+        <Route path="/" element={<Outlet />}>
+          <Route index element={<Items />} />
+          <Route path="/items" element={<Items />} />
+          <Route path="*" element={<div>페이지를 찾을 수 없습니다.</div>} />
+          <Route path="/additem" element={<AddItem />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
