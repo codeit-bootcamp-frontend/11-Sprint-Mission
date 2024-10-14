263a9e10b946d332df51f119c09703690aaee42f
kimdohoon2 <88535144+kimdohoon2@users.noreply.github.com>
2024-10-14 22:36:28 +0900

    feat : 반응형일때 ui변경
    - 모바일 버전 아이콘 추가

--- sprint-mission5/src/components/DropDownbtn.jsx
+++ sprint-mission5/src/components/DropDownbtn.jsx
@@ -1,13 +1,24 @@
 import "./DropDownbtn.css";
 import downIcon from "../images/dropdown/downicon.png";
+import Mobiledowniocn from "../images/dropdown/m_downicon.png";
 
-const DropDownbtn = ({ order, isOpen, toggleDropdown, handleOrderChange }) => {
+const DropDownbtn = ({
+  orderBy,
+  isOpen,
+  toggleDropdown,
+  handleOrderChange,
+}) => {
   return (
     <div className="dd-con">
       <div className="dd-box" onClick={toggleDropdown}>
-        <p>{order === "recent" ? "최신순" : "좋아요순"}</p>
+        <p>{orderBy === "recent" ? "최신순" : "좋아요순"}</p>
         <div className="d-icon">
-          <img src={downIcon} alt="다운아이콘" />
+          <img className="pc-downicon" src={downIcon} alt="PC다운아이콘" />
+          <img
+            className="m-downicon"
+            src={Mobiledowniocn}
+            alt="모바일다운아이콘"
+          />
         </div>
       </div>
       {isOpen && (
