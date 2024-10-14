4c39abdf783a2426ace3f83f12b63b53f41b6d24
JunHee <z1top159@gmail.com>
2024-10-14 10:53:39 +0900

    feat: 환경 변수 사용

--- src/api.js
+++ src/api.js
@@ -1,4 +1,4 @@
-const BASE_URL = "https://panda-market-api.vercel.app";
+const BASE_URL = process.env.REACT_APP_BASE_URL;
 
 export async function getProducts(params = {}) {
   const query = new URLSearchParams(params).toString();
