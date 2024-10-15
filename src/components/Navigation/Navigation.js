1ddb8dee9e3237bb1d8455330cf48d993868e2a9
WooLegend <dnwogus4260@naver.com>
2024-10-16 01:01:03 +0900

    feat: Link, NavLink를 활용해 Navigation 컴포넌트 개선

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,51 +1,47 @@
 import "./Navigation.css";
 import logoIcon from "../../assets/images/logo_icon.svg";
 import profile from "../../assets/images/profile.svg";
+import { Link, NavLink } from "react-router-dom";
 
-const tabs = [
+const TAB_DATAS = [
   { id: 0, name: "자유게시판", path: "/borad" },
-  { id: 1, name: "중고마켓", path: ["/items", "/additem"] },
+  { id: 1, name: "중고마켓", path: "/items" },
 ];
 
 function Navigation() {
   return (
     <nav className="Navigation">
       <div className="Navigation-wrap">
-        <a href="/" className="logo-wrap">
+        <Link to="/" className="logo-wrap">
           <img
             className="logo-icon"
             src={logoIcon}
             alt="판다마켓 아이콘 로고"
           />
           <span className="logo-text">판다마켓</span>
-        </a>
+        </Link>
         <ul className="Navigation-tabs">
-          {tabs.map((tab) => (
-            <li key={tab.id}>
-              <Tab path={tab.path}>{tab.name}</Tab>
+          {TAB_DATAS.map((data) => (
+            <li key={data.id}>
+              <Tab data={data} />
             </li>
           ))}
         </ul>
-        <a className="profile" href="/">
+        <Link className="profile" href="/">
           <img src={profile} alt="유저 프로필" />
-        </a>
+        </Link>
       </div>
     </nav>
   );
 }
 
-function Tab({ path, children }) {
-  const currnetPath = window.location.pathname;
-  const isCurrent = Array.isArray(path)
-    ? path.some((e) => currnetPath === e)
-    : currnetPath === path;
-
-  const classNames = `tab ${isCurrent ? "current" : ""}`;
-
+function Tab({ data }) {
+  const handleTabActive = ({ isActive }) =>
+    isActive ? { color: "#3692ff" } : {};
   return (
-    <a className={classNames} href={path[0]}>
-      <span>{children}</span>
-    </a>
+    <NavLink to={data.path} className="tab" style={handleTabActive}>
+      <span>{data.name}</span>
+    </NavLink>
   );
 }
 
