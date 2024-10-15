488c396d477ddbcc6e84e80c34f05c77983772a2
홍지윤 <h_zhirun@naver.com>
2024-10-15 21:49:54 +0900

    C:/Program Files/Git/additem에서 중고마켓 버튼 색상 활성화 및 css 수정

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,15 @@
-import { NavLink } from 'react-router-dom';
+import { NavLink, useLocation } from 'react-router-dom';
 import './Navbar.css';
 import logoImg from '../image/login-logo.png';
 import profileImg from '../image/profile-img.png';
 
-function getLinkStyle({ isActive }) {
-  return {
-    color: isActive ? '#3692ff' : undefined,
-  };
-}
-
 function Navbar() {
+  const location = useLocation();
+  // 파라미터로 경로를 받아, 현재 경로(location.pathname)가 포함되어 있는지 여부
+  const getLinkStyle = (targetPaths) => ({
+    color: targetPaths.includes(location.pathname) ? '#3692FF' : undefined,
+  });
+
   return (
     <div className="navbar">
       <div className="nav-wrapper">
@@ -17,10 +17,18 @@ function Navbar() {
           <img src={logoImg} alt="로고" />
         </div>
         <div className="navbar-menus">
-          <NavLink to="/freeboard" style={getLinkStyle} className="navbar-menu">
+          <NavLink
+            to="/freeboard"
+            style={() => getLinkStyle('/freeboard')}
+            className="navbar-menu"
+          >
             자유게시판
           </NavLink>
-          <NavLink to="/items" style={getLinkStyle} className="navbar-menu">
+          <NavLink
+            to="/items"
+            style={() => getLinkStyle(['/items', '/additem'])}
+            className="navbar-menu"
+          >
             중고마켓
           </NavLink>
         </div>
