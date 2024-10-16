80d346f9955638d851fc45bb47fdb834105dd76c
JunHee <z1top159@gmail.com>
2024-10-16 10:46:27 +0900

    fix: item.createdAt이 존재할 경우에만 dateOnly를 설정하도록 수정

--- src/api.js
+++ src/api.js
@@ -19,3 +19,23 @@ export async function getProductDetail(productId) {
     throw new Error("상품 상세 정보를 불러오는데 실패했습니다.");
   }
 }
+
+export async function getProductComments(productId, limit = 10, cursor = null) {
+  try {
+    const params = {
+      limit,
+    };
+
+    if (cursor) {
+      params.cursor = cursor;
+    }
+
+    const response = await instance.get(`/products/${productId}/comments`, {
+      params,
+    });
+
+    return response.data;
+  } catch (error) {
+    throw new Error("댓글 정보를 불러오는데 실패했습니다.");
+  }
+}
