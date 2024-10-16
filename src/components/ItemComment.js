b52d5d0ea80084affbb3fef9ebee4cee8afd9bdf
JunHee <z1top159@gmail.com>
2024-10-16 12:40:16 +0900

    style: 상세 페이지 정보 css파일 추가

---
+++ src/components/ItemComment.js
@@ -0,0 +1,11 @@
+function ItemComments() {
+  return (
+    <div>
+      문의하기
+      <div>문의하기 input</div>
+      <div className="item-comment"> 댓글</div>
+    </div>
+  );
+}
+
+export default ItemComments;
--- src/components/ItemDetailForm.js
+++ src/components/ItemDetailForm.js
@@ -2,6 +2,11 @@ import { useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import { getProductDetail } from "../api";
 import favoriteIcon from "../assets/Icon.svg";
+import ItemComments from "./ItemComment";
+import profileIcon from "../assets/Frame 2609463.png";
+import moreIcon from "../assets/Group 33735.svg";
+import backIcon from "../assets/ic_back.svg";
+import "./ItemDetailForm.css";
 
 function ItemDetailForm() {
   const { productId } = useParams();
@@ -46,65 +51,108 @@ function ItemDetailForm() {
   }
 
   return (
-    <div className="item-detail-container">
-      <div className="item-detail-images">
-        {item.images && item.images.length > 0 ? (
-          item.images.map((image, index) => (
-            <img
-              key={index}
-              src={image}
-              alt={`상품 이미지 ${index + 1}`}
-              className="item-detail-image"
-            />
-          ))
-        ) : (
-          <div>이미지가 없습니다.</div>
-        )}
-      </div>
+    <div className="item-detail">
+      <div className="item-detail-container">
+        <div className="item-detail-top-container">
+          <div className="item-detail-top">
+            <div className="item-detail-images">
+              {item.images && item.images.length > 0 ? (
+                item.images.map((image, index) => (
+                  <img
+                    key={index}
+                    src={image}
+                    alt={`상품 이미지 ${index + 1}`}
+                    className="item-detail-image"
+                  />
+                ))
+              ) : (
+                <div>이미지가 없습니다.</div>
+              )}
+            </div>
 
-      <div className="item-detail-info">
-        <div className="item-detail-title">
-          <div className="item-detail-name">{item.name}</div>
-          <div className="item-detail-price">
-            {item.price.toLocaleString()}원
+            <div className="item-detail-info">
+              <div className="item-detail-info-top">
+                <div className="item-detail-title">
+                  <div className="item-detail-name-container">
+                    <div className="item-detail-name">{item.name}</div>
+                    <img
+                      src={moreIcon}
+                      className="item-detail-more"
+                      alt="더보기 아이콘"
+                    />
+                  </div>
+                  <div className="item-detail-price">
+                    {item.price.toLocaleString()}원
+                  </div>
+                </div>
+                <div className="item-detail-introduce">
+                  <div className="item-detail-introduce-title">상품 소개</div>
+                  <p className="item-detail-description">{item.description}</p>
+                </div>
+              </div>
+
+              <div className="item-detail-info-bottom">
+                <div className="item-detail-tags-container">
+                  <div className="item-detail-tags-title">상품 태그</div>
+                  <div className="item-detail-tags">
+                    {" "}
+                    {item.tags && item.tags.length > 0 ? (
+                      item.tags.map((tag, index) => (
+                        <span key={index} className="item-detail-tag">
+                          #{tag}
+                        </span>
+                      ))
+                    ) : (
+                      <div>태그가 없습니다.</div>
+                    )}
+                  </div>
+                </div>
+
+                <div className="item-detail-author">
+                  <div className="item-detail-author-info">
+                    <img
+                      src={profileIcon}
+                      className="item-detail-author-image"
+                      alt="프로필사진"
+                    />
+                    <div className="item-detail-author-end">
+                      <div className="item-detail-author-nickname">
+                        {item.ownerNickname}
+                      </div>
+                      <div className="item-detail-createdAt">{dateOnly}</div>
+                    </div>
+                  </div>
+                  <div className="item-detail-favorite">
+                    {" "}
+                    <img
+                      src={favoriteIcon}
+                      className="item-detail-favorite-icon"
+                      alt="좋아요 아이콘"
+                    />
+                    <div className="item-detail-favorite-count">
+                      {item.favoriteCount}
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
 
-        <div className="item-detail-introduce">상품 소개</div>
-        <p className="item-detail-description">{item.description}</p>
-
-        <div className="item-detail-tags-title">상품 태그</div>
-        <div className="item-detail-tags">
-          {" "}
-          {item.tags && item.tags.length > 0 ? (
-            item.tags.map((tag, index) => (
-              <span key={index} className="item-detail-tag">
-                #{tag}
-              </span>
-            ))
-          ) : (
-            <div>태그가 없습니다.</div>
-          )}
-        </div>
-
-        <div className="item-detail-author-info">
-          <div className="item-detail-author-nickname">
-            {item.ownerNickname}
+        <div className="item-detail-bottom-container">
+          <div className="item-detail-bottom">
+            <div className="item-detail-comments">
+              <ItemComments />
+            </div>
           </div>
-          <div className="item-detail-createdAt">{dateOnly}</div>
+
+          <div className="item-detail-button-container">
+            <button onClick={handleGoBack} className="item-detail-back-button">
+              목록으로 돌아가기
+              <img src={backIcon} className="back-icon" alt="뒤로가기 아이콘" />
+            </button>
+          </div>
         </div>
-
-        <div className="item-detail-favorite">
-          {" "}
-          <img src={favoriteIcon} alt="좋아요 아이콘" />
-          {item.favoriteCount}
-        </div>
-
-        <div className="item-detail-comments">문의하기</div>
-
-        <button onClick={handleGoBack} className="item-detail-back-button">
-          목록으로 돌아가기
-        </button>
       </div>
     </div>
   );
