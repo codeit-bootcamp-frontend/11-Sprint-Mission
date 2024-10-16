d83c0ea5cb482386e6dac975dfd0438ee2e574ae
1022gusl <1022gusl@gmail.com>
2024-10-16 20:57:18 +0900

    form preventDefault add

--- src/pages/AddItemPage/AddItemPage.jsx
+++ src/pages/AddItemPage/AddItemPage.jsx
@@ -4,20 +4,26 @@ import InputField from "./components/InputField";
 import ImageUpload from "./components/ImageUpload";
 import InputTag from "./components/InputTag";
 
+/* 현재 추가를 해야 하는 것은 반응형디자인과 태그기능 이미지딜리트기능 태그딜리트기능 등록버튼 스테이트 */
+
 function AddItem() {
   const [productName, setProductName] = useState("");
   const [productIntroduction, setProductIntroduction] = useState("");
   const [productPrice, setProductPrice] = useState("");
 
+  const handleSubmit = (e) => {
+    e.preventDefault(); // 폼 제출 방지
+  };
+
   return (
     <div className="container">
-      <form>
+      <form onSubmit={handleSubmit}>
         <section className="submitSection">
           <h1>상품 등록하기</h1>
           <button className="submitButton">등록</button>
+          {/* 아직 기능을 다 추가하지 않아서 등록버튼에 대한 스테이트를 만들지 않았습니다! */}
         </section>
         <section className="inputSection">
-          {/* 이미지는 아직 미구현 */}
           <ImageUpload title="상품 이미지" />
 
           <InputField
@@ -46,7 +52,7 @@ function AddItem() {
             value={productPrice}
             onChange={(e) => setProductPrice(e.target.value)}
           />
-          {/* 태그는 아직 미구현 */}
+          {/* 태그는 아직 구현중 */}
           <InputTag />
         </section>
       </form>
