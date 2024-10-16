48be9c857d1a9b295e1d1b49c377964ebcd6bafe
haksoo <haksoo0918@gmail.com>
2024-10-16 12:52:32 +0900

    설정: eslint 추가해봄

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
+    // npm install --save-dev eslint-config-prettier
+    // 'prettier',
+  ],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react'],
+  rules: {
+    'react/prop-types': 'off',
+  },
+};
