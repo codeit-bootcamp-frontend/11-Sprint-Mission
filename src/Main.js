cd15fa5053ced17eaf342419bbe05af3d889cc74
haksoo <haksoo0918@gmail.com>
2024-10-16 12:55:09 +0900

    재구성: 홈 추가 및 outlet 으로 변경

--- src/Main.js
+++ src/Main.js
@@ -2,17 +2,19 @@ import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 import App from './App';
 import Items from './Pages/Items';
 import AddItem from './Pages/AddItem';
+import Home from './Pages/Home';
 
 function Main() {
   return (
     <BrowserRouter>
-      <App>
-        <Routes>
+      <Routes>
+        <Route path="/" element={<App />}>
+          <Route index element={<Home />} />
           <Route path="/items" element={<Items />} />
           <Route path="/additem" element={<AddItem />} />
-          <Route path="*" element={<Navigate to="/items" />} />
-        </Routes>
-      </App>
+          <Route path="*" element={<Navigate to="/" />} />
+        </Route>
+      </Routes>
     </BrowserRouter>
   );
 }
