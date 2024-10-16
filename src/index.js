44a32be1b171fbfc3b33876496e8f347809e94ce
haksoo <haksoo0918@gmail.com>
2024-10-16 12:57:02 +0900

    추가: strict mode 가 권장사항이길레 넣음

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
+import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Main from './Main';
 import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<Main />);
+root.render(
+  <React.StrictMode>
+    <Main />
+  </React.StrictMode>
+);
