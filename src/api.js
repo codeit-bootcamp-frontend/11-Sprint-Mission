ed8930f96e3124493d7bae5a0d2db9a610379e78
WooLegend <dnwogus4260@naver.com>
2024-10-19 01:07:10 +0900

    feat: 상품 상세 조회 API 개발

--- src/api.js
+++ src/api.js
@@ -3,12 +3,12 @@
 const BASE_URL = "https://panda-market-api.vercel.app";
 
 /**
- * 상품 검색 API
- * @param {Number} page 페이지 번호 (기본 : 1)
- * @param {Number} pageSize 페이지당 상품 수 (기본 : 12)
- * @param {String} orderBy recent 최신순(기본) / favorite 좋아요순
- * @param {String} keyword 검색 키워드
- * @returns 상품 리스트 객체 반환
+ * 상품 목록 조회 API
+ * @param {number} page 페이지 번호 (기본 : 1)
+ * @param {number} pageSize 페이지당 상품 수 (기본 : 12)
+ * @param {string} orderBy recent 최신순(기본) / favorite 좋아요순
+ * @param {string} keyword 검색 키워드
+ * @returns {object} 상품 목록 객체
  */
 export async function getProducts(
   page = 1,
@@ -23,6 +23,18 @@ export async function getProducts(
   url.searchParams.append("orderBy", orderBy);
   keyword && url.searchParams.append("keyword", keyword);
 
+  const res = await fetch(url.href);
+  return res.json();
+}
+
+/**
+ * 상품 상세 조회 API
+ * @param {string} id
+ * @returns {object} 상품 상세 정보 객체
+ */
+export async function getProductById(id) {
+  if (!BASE_URL) throw new Error("요청을 보낼 수 없습니다.");
+  const url = new URL(BASE_URL + "/products/" + id);
   const res = await fetch(url.href);
   return res.json();
 }
