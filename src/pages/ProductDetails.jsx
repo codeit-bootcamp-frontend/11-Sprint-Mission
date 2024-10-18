64a4a7afb5e9d655dedaaae507f9db56449e860d
양정화 <siltarre@gmail.com>
2024-10-19 00:36:17 +0900

    textarea height 스타일링

--- src/pages/ProductDetails.jsx
+++ src/pages/ProductDetails.jsx
@@ -80,7 +80,7 @@ function ProductDetails() {
           <h4>문의하기</h4>
           <form>
             <textarea
-              className="default"
+              className={`default ${styled["inquiry-input"]}`}
               value={formValues}
               name="inquiry"
               placeholder="개인정보를 공유 및 요청하거나, 명예 훼손, 무단 광고, 불법 정보 유포시 모니터링 후 삭제될 수 있으며, 이에 대한 민형사상 책임은 게시자에게 있습니다."
