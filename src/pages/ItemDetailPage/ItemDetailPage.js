bcfc1c3e8da2378d11e172b90d6160a4219590dd
WooLegend <dnwogus4260@naver.com>
2024-10-19 15:31:05 +0900

    feat: 상품 상세 페이지에서 상세 데이터 가져오기 개발

--- src/pages/ItemDetailPage/ItemDetailPage.js
+++ src/pages/ItemDetailPage/ItemDetailPage.js
@@ -1,8 +1,22 @@
 import { useParams } from "react-router-dom";
+import { useDeviceType } from "../../contexts/DeviceTypeContext";
+import { useEffect, useState } from "react";
+import { getProductById } from "../../api";
 
 function ItemDetailPage() {
+  const [item, setItem] = useState();
   const { id } = useParams();
-  return <>{id}</>;
+  const deviceType = useDeviceType();
+
+  useEffect(() => {
+    const fetchDate = async () => {
+      const result = await getProductById(id);
+      setItem(result);
+    };
+    fetchDate();
+  }, [id]);
+
+  return <>{item.name}</>;
 }
 
 export default ItemDetailPage;
