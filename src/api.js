f28563384b423268aa619fefd329efdbd09ff1b7
Jeongbin1 <petitjb05@gamil.com>
2024-10-19 10:28:39 +0900

    refactor: get요청 에러처리

--- src/api.js
+++ src/api.js
@@ -4,8 +4,12 @@ export async function getProducts({ order = "recent" }) {
   const query = `order=${order}`;
   const response = await fetch(`${BASE_URL}/products?${query}`);
   if (!response.ok) {
-    throw new Error("상품을 불러오는데 실패했습니다");
+    throw new Error(`상품을 불러오는데 실패했습니다. (${response.status} 에러)`);
   }
-  const body = await response.json();
-  return body;
+  try {
+    const body = await response.json();
+    return body;
+  } catch (error) {
+    throw new Error("서버 응답을 처리하는데 실패했습니다");
+  }
 }
