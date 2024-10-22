d56f8a17ae990114e7116637d0cfc5eaf1e4bb95
1022gusl <1022gusl@gmail.com>
2024-10-23 02:35:29 +0900

    added heartButton

--- src/pages/ItemDetailPage/components/ItemInfo.jsx
+++ src/pages/ItemDetailPage/components/ItemInfo.jsx
@@ -1,29 +1,35 @@
 import React from "react";
 import "./ItemInfo.css";
+import { ReactComponent as HeartIcon } from "../../../images/icons/heart.svg";
 
 const ItemInfo = ({ product }) => {
   return (
     <div className="itemInfo">
       <img className="itemImage" src={product.images[0]} alt={product.name} />
       <div className="itemDetailContainer">
-        <div className="itemTitle">
-          <h1 className="itemName">{product.name}</h1>
-          <h1 className="itemPrice">{product.price.toLocaleString()}원</h1>
+        <div className="itemDetailContentContainer">
+          <div className="itemTitle">
+            <h1 className="itemName">{product.name}</h1>
+            <h1 className="itemPrice">{product.price.toLocaleString()}원</h1>
+          </div>
+          <div className="descriptionContainer">
+            <h3>상품 소개</h3>
+            <p className="itemDescription">{product.description}</p>
+          </div>
+          <div className="itemTags">
+            <h3 className="tagTitle">상품 태그</h3>
+            {product.tags.map((tag) => (
+              <span key={tag} className="tag">
+                #{tag}
+              </span>
+            ))}
+          </div>
         </div>
-        <div className="descriptionContainer">
-          <h3>상품 소개</h3>
-          <p className="itemDescription">{product.description}</p>
-        </div>
-        <div className="itemTags">
-          <h3 className="tagTitle">상품 태그</h3>
-          {product.tags.map((tag) => (
-            <span key={tag} className="tag">
-              #{tag}
-            </span>
-          ))}
-          {/* 반응형 디자인 조금 더 고려해야함 아직 미완성 
-          그리고 하트버튼과 사용자 정보?는 컴포넌트로 추가할 예정
-          */}
+        <div className="heartButtonContainer">
+          <button className="heartButton">
+            <HeartIcon className="heartIcon" />
+            {product.favoriteCount}
+          </button>
         </div>
       </div>
     </div>
