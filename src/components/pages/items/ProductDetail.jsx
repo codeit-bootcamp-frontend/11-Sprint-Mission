552a54a0286bf04251c2a0a472482cf7d5a1fa23
Donghunn-Lee <93193485+Donghunn-lee@users.noreply.github.com>
2024-10-22 18:59:11 +0900

    feat: ProductDatail 페이지 상품 정보 구현
    - 상품 정보 디자인 추가

--- src/components/pages/items/ProductDetail.jsx
+++ src/components/pages/items/ProductDetail.jsx
@@ -1,8 +1,9 @@
-import { useParams } from 'react-router-dom';
+import { useParams, Link } from 'react-router-dom';
 import Header from '../../common/home/Header';
 import { useEffect, useState } from 'react';
 import { getProductById } from '../../../api/items/productsApi';
-import { Link } from 'react-router-dom';
+import './ProductDetail.css';
+import AddItemTag from '../additem/AddItemTag';
 
 function ProductDetail() {
   const { id } = useParams();
@@ -34,6 +35,18 @@ function ProductDetail() {
     return <p>상품을 찾을 수 없습니다.</p>;
   }
 
+  const getFormattedDate = (isoString) => {
+    const date = new Date(isoString);
+
+    const year = date.getFullYear();
+    const month = String(date.getMonth() + 1).padStart(2, '0');
+    const day = String(date.getDate()).padStart(2, '0');
+
+    const formattedDate = `${year}. ${month}. ${day}`;
+
+    return formattedDate;
+  };
+
   return (
     <>
       <Header
@@ -53,12 +66,57 @@ function ProductDetail() {
           </Link>
         }
       />
-      <div className="product-detail">
-        <img src={product.images[0]} alt={product.name} />
-        <h1>{product.name}</h1>
-        <p>{product.price.toLocaleString()}원</p>
-        <p>좋아요 수: {product.favoriteCount}</p>
-      </div>
+      <main className="product-detail-container">
+        <div className="product-detail-content">
+          <img
+            className="product-detail-image"
+            src={product.images[0]}
+            alt={product.name}
+          />
+          <div className="product-detail-info-wrapper">
+            <div className="product-detail-info">
+              <div className="product-detail-info-title">
+                <h1 className="product-detail-info-name">{product.name}</h1>
+                <p className="product-detail-info-price">
+                  {product.price.toLocaleString()}원
+                </p>
+                <img className='product-detail-info-kebab' src="/images/icons/ic_kebab.svg" alt="상품 게시글 추가 기능 아이콘" />
+              </div>
+              <div className="product-detail-info-description-wrapper">
+                <div className="product-detail-info-description">
+                  <p className="product-detail-info-caption">상품 소개</p>
+                  <p className="product-detail-info-description-text">
+                    {product.description}
+                  </p>
+                </div>
+                <div className="product-detail-info-tag">
+                  <p className="product-detail-info-caption">상품 태그</p>
+                  <AddItemTag tags={product.tags} />
+                </div>
+              </div>
+              <div className="product-detail-seller">
+                <div className="seller-info">
+                  <img
+                    className="seller-image"
+                    src="/images/icons/ic_mypage.svg"
+                    alt="판매자 이미지"
+                  />
+                  <div className="seller-wrapper">
+                    <p className="seller-nickname">{product.ownerNickname}</p>
+                    <p className="seller-updatedat">
+                      {getFormattedDate(product.updatedAt)}
+                    </p>
+                  </div>
+                </div>
+                <div className="seller-favorite-wrapper">
+                  <img className='seller-favorite-image' src="/images/icons/ic_heart.svg" alt="좋아요 이미지" />
+                  <p className='seller-favorite-count'>{product.favoriteCount}</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </main>
     </>
   );
 }
