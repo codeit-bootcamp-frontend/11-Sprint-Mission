4e421b212eff00cb83f31080d6b5d617a3c58d3b
홍지윤 <h_zhirun@naver.com>
2024-11-16 22:37:14 +0900

    목록으로 돌아가기 버튼 구현

--- src/pages/ItemDetailPage.js
+++ src/pages/ItemDetailPage.js
@@ -1,14 +1,25 @@
 import ProductComment from "../components/ProductComment";
 import ProductDetail from "../components/ProductDetail";
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
+import "../styles/ItemDetailPage.css";
 
 function ItemDetailPage() {
   const { productId } = useParams();
+  const navigate = useNavigate();
+
+  const handleBackToList = () => {
+    navigate("/items");
+  };
 
   return (
     <>
       <ProductDetail productId={productId} />
       <ProductComment productId={productId} />
+      <div className="button-container">
+        <button className="back-to-list" onClick={handleBackToList}>
+          목록으로 돌아가기 ↩
+        </button>
+      </div>
     </>
   );
 }
