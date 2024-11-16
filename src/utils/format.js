b36ef6f75c1c70cdf15eaa62ad0a613073d9a92d
tare <59001439+junghwa1996@users.noreply.github.com>
2024-11-16 23:06:45 +0900

    ✨ 날짜 변환함수 utils format으로 추가

--- src/utils/format.js
+++ src/utils/format.js
@@ -1,3 +1,23 @@
 export const formatPrice = (price) => {
+  if (price == null) {
+    return '0';
+  }
   return price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+};
+
+export const formatRegistrationDate = (isDate) => {
+  const registrationDate = new Date(isDate);
+  const currentDate = new Date();
+  const timeDifference = currentDate - registrationDate;
+  const hoursDifference = timeDifference / (1000 * 60 * 60);
+
+  if (hoursDifference < 24) {
+    const roundedHours = Math.floor(hoursDifference);
+    return roundedHours < 1 ? '방금 전' : `${roundedHours}시간 전`;
+  } else {
+    const year = registrationDate.getFullYear();
+    const month = String(registrationDate.getMonth() + 1).padStart(2, '0');
+    const day = String(registrationDate.getDate()).padStart(2, '0');
+    return `${year} .${month} .${day}`;
+  }
 };
