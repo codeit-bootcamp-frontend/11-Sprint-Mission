f8b07991329c3dec28a22095efe593af902dd3a4
whdjh <whdjh98@naver.com>
2024-11-23 04:10:13 +0900

    feat: App, index 마이그레이션

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,11 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./styles/global.css";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const rootElement = document.getElementById("root") as HTMLElement;
+
+const root = ReactDOM.createRoot(rootElement);
+
 root.render(
   <App />
-); 
+);
