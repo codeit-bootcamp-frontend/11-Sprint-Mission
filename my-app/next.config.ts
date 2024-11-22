ea9c3e7d2ef3afb902b311d57dad278e374ea11d
kimdohoon2 <88535144+kimdohoon2@users.noreply.github.com>
2024-11-22 23:27:16 +0900

    feat : 외부이미지 추가하기위해 코드추가

--- my-app/next.config.ts
+++ my-app/next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    domains: ["sprint-fe-project.s3.ap-northeast-2.amazonaws.com"],
+  },
 };
 
 export default nextConfig;
