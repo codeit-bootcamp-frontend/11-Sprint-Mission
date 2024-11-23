7567b5124ba25f85c43c70995f684779840eecd1
WooLegend <dnwogus4260@naver.com>
2024-11-24 05:42:31 +0900

    feat: 디바이스 타입 Provider 구현

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import type { AppProps } from "next/app";
 import Head from "next/head";
 import "../styles/global.css";
+import { DeviceTypeProvider } from "@/contexts/DeviceTypeContext";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
@@ -11,7 +12,9 @@ export default function App({ Component, pageProps }: AppProps) {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/images/ic_logo.svg" />
       </Head>
-      <Component {...pageProps} />
+      <DeviceTypeProvider>
+        <Component {...pageProps} />
+      </DeviceTypeProvider>
     </>
   );
 }
