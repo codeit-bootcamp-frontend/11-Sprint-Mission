fe668eba27b1b6f4b257ba59e1242098248b5f2c
kimdohoon2 <88535144+kimdohoon2@users.noreply.github.com>
2024-11-23 17:42:55 +0900

    feat : 게시글 최신순과 좋아요순을 불러오는 ui완성

---
+++ my-app/src/app/components/AllItem.tsx
@@ -0,0 +1,42 @@
+import Image from "next/image";
+import { BestItemData } from "../type";
+
+export default function AllItem({
+  title,
+  writer,
+  likeCount,
+  createdAt,
+  image,
+}: BestItemData) {
+  return (
+    <>
+      <div className="w-full h-[136px] border-b border-gray-300 mt-[16px]">
+        <div>
+          <div className="flex items-center justify-between">
+            <h2 className="text-lg font-bold">{title}</h2>
+            <div className="w-[72px] h-[72px] relative rounded-lg border border-gray200">
+              <Image src={image} fill alt={title} />
+            </div>
+          </div>
+          <div className="flex items-center justify-between mt-[15px] mb-[24px]">
+            <div className="flex items-center gap-[4px]">
+              <div className="w-[24px] h-[24px] relative">
+                <Image src="/head/myPageIcon.png" fill alt="myPageIcon" />
+              </div>
+              <p className="text-sm text-gray-400">{writer.nickname}</p>
+              <p className="text-sm text-gray-400">
+                {new Date(createdAt).toLocaleDateString()}
+              </p>
+            </div>
+            <div className="flex items-center gap-[8px]">
+              <div className="w-[21px] h-[18px] relative">
+                <Image src="/icon/like_icon.png" fill alt="likeicon" />
+              </div>
+              <p className="text-sm text-gray-400">{likeCount}</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
