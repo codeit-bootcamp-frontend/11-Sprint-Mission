27f835b5ead099b1a72852cfab7baf894a88a3fd
WooLegend <dnwogus4260@naver.com>
2024-11-24 00:13:29 +0900

    feat: 게시글 아이템 구조 구현

--- pages/board.tsx
+++ pages/board.tsx
@@ -1,10 +1,13 @@
+import { Article, ArticleList } from "@/types/Article";
 import Image from "next/image";
+
+const mock_article: ArticleList = { totalCount: 0, list: [] };
 
 export default function Board() {
   return (
     <>
       <BestPostBoard />
-      <PostBoard />
+      <PostBoard articles={mock_article} />
     </>
   );
 }
@@ -13,7 +16,7 @@ function BestPostBoard() {
   return null;
 }
 
-function PostBoard() {
+function PostBoard({ articles }: { articles: ArticleList }) {
   return (
     <div>
       <header>
@@ -44,6 +47,30 @@ function PostBoard() {
               alt="정렬"
             />
           </div>
+        </div>
+      </div>
+      <div>
+        {articles.list.map((article) => (
+          <PostBoardItem key={article.id} article={article} />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+function PostBoardItem({ article }: { article: Article }) {
+  return (
+    <div>
+      <div>
+        <div>
+          <Image
+            fill
+            src={article.image}
+            alt={article.title}
+            style={{
+              objectFit: "cover",
+            }}
+          />
         </div>
       </div>
     </div>
