2a748cbfea2f83781c12b0b4be949f8c0d25b183
OhJinHwa <80371898+0hjinhwa@users.noreply.github.com>
2024-11-23 16:11:30 +0900

    feat: NavBar 구현

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,16 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import NavBar from '@/components/NavBar';
+import '@/styles/globals.css';
+import Head from 'next/head';
+import type { AppProps } from 'next/app';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <div>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+      <NavBar />
+      <Component {...pageProps} />
+    </div>
+  );
 }
