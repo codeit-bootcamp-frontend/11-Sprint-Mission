b354221ea1bb70010ad166f5928001b7ad611c4f
haksoo <haksoo0918@gmail.com>
2024-11-23 14:27:16 +0900

    ✨ link에 active 추가, 스타일 변수 활용

--- components/Nav/index.tsx
+++ components/Nav/index.tsx
@@ -1,11 +1,20 @@
-import Image from 'next/image';
 import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+import clsx from 'clsx';
 //
 import styles from './Nav.module.css';
 import LogoImg from './logo.svg';
 import LogoTextImg from './txt-panda.svg';
 
+// nav links
+const navLinks = [
+  { href: '/boards', label: '자유게시판' },
+  { href: '/items', label: '중고마켓' },
+];
+
 export default function Nav() {
+  const pathname = usePathname();
+
   return (
     <header className={styles.header}>
       <div className={styles.container}>
@@ -16,16 +25,13 @@ export default function Nav() {
           </Link>
 
           <ul className={styles.links}>
-            <li>
-              <Link href="#/" className={styles.link}>
-                자유게시판
-              </Link>
-            </li>
-            <li>
-              <Link href="/" className={styles.link}>
-                중고마켓
-              </Link>
-            </li>
+            {navLinks.map(({ href, label }) => (
+              <li key={href}>
+                <Link href={href} className={clsx(styles.link, pathname === href && styles.active)}>
+                  {label}
+                </Link>
+              </li>
+            ))}
           </ul>
         </nav>
 
