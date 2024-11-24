8b7c72fa12d283ef57a9de326828155eeee78c0d
haksoo <haksoo0918@gmail.com>
2024-11-24 16:17:21 +0900

    💡 formatDate 주석 추가

--- lib/formatDate.ts
+++ lib/formatDate.ts
@@ -1,3 +1,9 @@
+/**
+ * 날짜 포맷팅
+ * @param {string | number} timestamp - 날짜 문자열 또는 숫자
+ * @param {string} joinText - 날짜 구분자
+ * @returns {string} 날짜 포맷팅 문자열
+ */
 const formatDate = (timestamp: string | number, joinText: string = '.'): string => {
   const d = new Date(timestamp);
   return d.toISOString().slice(0, 10).split('-').join(joinText);
