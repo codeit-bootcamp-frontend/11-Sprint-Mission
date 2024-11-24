3d7ed1d8471e3ae4eaf08d98d41310eb3e1b0d9e
k-jipgeria <sgim43547@gmail.com>
2024-11-24 11:34:48 +0900

    chore: not-found page 한 문단 삭제

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -6,9 +6,6 @@ const NotFoundPage = () => {
       <Header />
       <div className="flex flex-col items-center justify-center h-screen">
         <h1 className="text-4xl font-bold mb-4">페이지를 찾을 수 없습니다!</h1>
-        <p className="mt-4 text-lg">
-          죄송합니다, 요청하신 페이지가 존재하지 않습니다.
-        </p>
       </div>
     </>
   );
