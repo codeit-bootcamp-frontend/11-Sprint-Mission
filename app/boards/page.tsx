0f5810ae6503001a0dd5855519407dfc31138bbd
1022gusl <1022gusl@gmail.com>
2024-11-26 02:11:22 +0900

    feat: 서스펜즈 감싸기

--- app/boards/page.tsx
+++ app/boards/page.tsx
@@ -1,3 +1,4 @@
+import { Suspense } from "react";
 import AllPost from "../components/boards/AllPost";
 import BestPost from "../components/boards/BestPost";
 
@@ -5,7 +6,9 @@ export default function boards() {
   return (
     <>
       <BestPost />
-      <AllPost />
+      <Suspense fallback={<div>로딩 중</div>}>
+        <AllPost />
+      </Suspense>
     </>
   );
 }
