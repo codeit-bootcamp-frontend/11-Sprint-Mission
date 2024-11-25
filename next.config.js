3b73d0129260b724987b1ec2aac2ee2465b14df8
1022gusl <1022gusl@gmail.com>
2024-11-26 00:34:33 +0900

    feat: 글꼴 적용 및 경고 메세지 개선

--- next.config.js
+++ next.config.js
@@ -2,12 +2,27 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: [
-      "example.com",
-      "sprint-fe-project.s3.ap-northeast-2.amazonaws.com",
-      "http://via.placeholder.com/500.jpg/",
-      "via.placeholder.com",
-      "flexible.img.hani.co.kr",
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "example.com",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "sprint-fe-project.s3.ap-northeast-2.amazonaws.com",
+        pathname: "/**",
+      },
+      {
+        protocol: "http",
+        hostname: "via.placeholder.com",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "flexible.img.hani.co.kr",
+        pathname: "/**",
+      },
     ],
   },
 };
