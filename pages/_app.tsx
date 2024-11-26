c81d0094bd015895352a1528826a8730cd305541
whdjh <whdjh98@naver.com>
2024-11-26 11:02:56 +0900

    변경사항커밋

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,7 @@
 // 'App' 구성 요소에 대해 특별히 규칙을 비활성화
 /* eslint-disable react/jsx-props-no-spreading */
 
-import "@/styles/globals.css";
+import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
 
 export default function App({ Component, pageProps }: AppProps) {
