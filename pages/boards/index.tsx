216e8959b44ad388a9fd72bcb0010a8ef030489c
장수경 <jangsookyeong@jangsoogyeong-ui-MacBookAir.local>
2024-11-26 17:21:40 +0900

    boards index.tsx 수정

--- pages/boards/index.tsx
+++ pages/boards/index.tsx
@@ -1,1 +1,30 @@
+import React from "react";
+import BestArticle from "../../components/boards/BestArticle";
+import AllArticle from "../../components/boards/AllArticle";
+import { Article, ArticleList } from "@/types/Types";
 
+export async function getServerSideProps() {
+  const response = await fetch(
+    `https://panda-market-api.vercel.app/articles?orderBy=recent`
+  );
+  const data: ArticleList = await response.json();
+
+  return {
+    props: {
+      initialArticles: data.list,
+    },
+  };
+}
+
+interface BoardsPageProps {
+  initialArticles: Article[];
+}
+
+export default function BoardsPage({ initialArticles }: BoardsPageProps) {
+  return (
+    <div className="pageContainer">
+      <BestArticle />
+      <AllArticle initialArticles={initialArticles} />
+    </div>
+  );
+}
