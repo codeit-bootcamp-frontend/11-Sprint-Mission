d1094f5f49a23956a87a2c19ee3c92dd40980e50
장수경 <jangsookyeong@jangsoogyeong-ui-MacBookAir.local>
2024-11-26 17:40:02 +0900

    .

--- components/productdetail/ItemContent.tsx
+++ components/productdetail/ItemContent.tsx
@@ -1,67 +1,67 @@
-// import React from "react";
-// import TagList from "./TagList";
-// import LikeButton from "./LikeButton";
-// import { ReactComponent as Kebab } from "../../../images/ic_kebab.svg";
+import React from "react";
+import TagList from "./TagList";
+import LikeButton from "./LikeButton";
+import { ReactComponent as Kebab } from "../../../images/ic_kebab.svg";
 
-// interface Product {
-//   id: string;
-//   name: string;
-//   description: string;
-//   price: number;
-//   tags: string[];
-//   images: string[];
-//   isFavorite: boolean;
-//   favoriteCount: number;
-// }
+interface Product {
+  id: string;
+  name: string;
+  description: string;
+  price: number;
+  tags: string[];
+  images: string[];
+  isFavorite: boolean;
+  favoriteCount: number;
+}
 
-// interface ItemContentProps {
-//   product: Product;
-// }
+interface ItemContentProps {
+  product: Product;
+}
 
-// function ItemContent({ product }: ItemContentProps) {
-//   return (
-//     <div className="sectionContainer">
-//       {/* 상품 대표 이미지 */}
-//       <div className="itemImage">
-//         <img src={product.images[0]} alt={`${product.name} 상품 대표 사진`} />
-//       </div>
+function ItemContent({ product }: ItemContentProps) {
+  return (
+    <div className="sectionContainer">
+      {/* 상품 대표 이미지 */}
+      <div className="itemImage">
+        <img src={product.images[0]} alt={`${product.name} 상품 대표 사진`} />
+      </div>
 
-//       <div className="itemDetailContainer">
-//         <div className="mainDetail">
-//           {/* 더보기 버튼 */}
-//           <button className="kebabButton">
-//             <Kebab className="kebobIcon" />
-//           </button>
+      <div className="itemDetailContainer">
+        <div className="mainDetail">
+          {/* 더보기 버튼 */}
+          <button className="kebabButton">
+            <Kebab className="kebobIcon" />
+          </button>
 
-//           <div>
-//             <h2 className="itemName">{product.name}</h2>
-//             {/* 가격 표시 */}
-//             <p className="itemPrice">{product.price.toLocaleString()}원</p>
-//           </div>
+          <div>
+            <h2 className="itemName">{product.name}</h2>
+            {/* 가격 표시 */}
+            <p className="itemPrice">{product.price.toLocaleString()}원</p>
+          </div>
 
-//           <hr />
+          <hr />
 
-//           <div>
-//             <h3 className="itemIntroduce">상품 소개</h3>
-//             <p className="description">{product.description}</p>
-//           </div>
+          <div>
+            <h3 className="itemIntroduce">상품 소개</h3>
+            <p className="description">{product.description}</p>
+          </div>
 
-//           <div className="tagSection">
-//             <h3 className="tag">상품 태그</h3>
-//             <TagList tags={product.tags} />
-//           </div>
-//         </div>
+          <div className="tagSection">
+            <h3 className="tag">상품 태그</h3>
+            <TagList tags={product.tags} />
+          </div>
+        </div>
 
-//         <LikeButton
-//           productId={product.id}
-//           isFavorite={product.isFavorite}
-//           favoriteCount={product.favoriteCount}
-//         />
-//       </div>
+        <LikeButton
+          productId={product.id}
+          isFavorite={product.isFavorite}
+          favoriteCount={product.favoriteCount}
+        />
+      </div>
 
-//       <hr />
-//     </div>
-//   );
-// }
+      <hr />
+    </div>
+  );
+}
 
-// export default ItemContent;
+export default ItemContent;
--- components/productdetail/TagList.tsx
+++ components/productdetail/TagList.tsx
@@ -1,17 +1,17 @@
-// import React from "react";
+import React from "react";
 
-// function TagList({ tags }) {
-//   if (!tags || tags.length === 0) return null;
+function TagList({ tags }) {
+  if (!tags || tags.length === 0) return null;
 
-//   return (
-//     <div>
-//       {tags.map((tag, index) => (
-//         <span key={`tag-display-${index}`} className="tag">
-//           #{tag}
-//         </span>
-//       ))}
-//     </div>
-//   );
-// }
+  return (
+    <div>
+      {tags.map((tag, index) => (
+        <span key={`tag-display-${index}`} className="tag">
+          #{tag}
+        </span>
+      ))}
+    </div>
+  );
+}
 
-// export default TagList;
+export default TagList;
