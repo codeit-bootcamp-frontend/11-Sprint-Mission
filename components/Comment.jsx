e7bfa9fb66abedb00863fa9ead9616df7fe81dcf
lemon1335 <kimjinhui114@gmail.com>
2024-11-29 18:24:00 +0900

    [feat] 댓글 리스트 조회 pc ui 구현 완료

--- components/Comment.jsx
+++ components/Comment.jsx
@@ -1,6 +1,7 @@
 import Image from 'next/image';
 import SelectBox from '@/components/common/SelectBox';
 import Profile from '@/public/ic_profile.svg';
+import styles from '@/styles/Comment.module.css';
 
 export default function Comment({ comment }) {
   const formatDate = value => {
@@ -10,15 +11,17 @@ export default function Comment({ comment }) {
   };
 
   return (
-    <div className="comment">
-      <SelectBox />
-      <div className="comment-container">
-        <div className="comment-content">{comment?.content}</div>
-        <div className="writer">
+    <div className={styles.commentCard}>
+      <div className={styles.commentContainer}>
+        <div className={styles.commentHeader}>
+          <div className={styles.commentContent}>{comment?.content}</div>
+          <SelectBox />
+        </div>
+        <div className={styles.commentWriter}>
           <Image src={comment?.writer.image || Profile} alt="프로필사진" className="profile-img" />
-          <div className="buyer-info">
-            <p className="buyer-nickname">{comment?.writer.nickname}</p>
-            <p className="comment-updatedAt">{formatDate(comment?.updatedAt)}</p>
+          <div className={styles.writerInfo}>
+            <p className={styles.writerNickname}>{comment?.writer.nickname}</p>
+            <p className={styles.commentUpdatedAt}>{formatDate(comment?.updatedAt)}</p>
           </div>
         </div>
       </div>
