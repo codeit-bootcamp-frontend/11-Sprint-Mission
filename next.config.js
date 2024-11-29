18449dbe7a78073e2baa15a772fd9e59f7da3a02
whdjh <whdjh98@naver.com>
2024-11-29 19:52:06 +0900

    초기설정찐막 끝

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: ['sprint-fe-project.s3.ap-northeast-2.amazonaws.com'],
+  },
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack'],
+    });
+    return config;
+  },
 };
 
 module.exports = nextConfig;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,14 @@
 // 'App' 구성 요소에 대해 특별히 규칙을 비활성화
 /* eslint-disable react/jsx-props-no-spreading */
 
-import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
+import GlobalStyle from '@/styles/global';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <GlobalStyle />
+      <Component {...pageProps} />
+    </>
+  );
 }
