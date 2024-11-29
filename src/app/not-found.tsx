081bcee60efff8da45efe717ee78bc5abcb1aee2
k-jipgeria <sgim43547@gmail.com>
2024-11-29 20:25:07 +0900

    feat: Add NotFound.tsx page for users who enter any url

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,14 +1,16 @@
-import Header from "@/components/Header";
+import Link from "next/link";
 
-const NotFoundPage = () => {
+export default function NotFound() {
   return (
-    <>
-      <Header />
-      <div className="flex flex-col items-center justify-center h-screen">
-        <h1 className="text-4xl font-bold mb-4">페이지를 찾을 수 없습니다!</h1>
-      </div>
-    </>
+    <div className="flex flex-col items-center justify-center min-h-screen">
+      <h2 className="text-2xl font-bold mb-4">페이지를 찾을 수 없습니다</h2>
+      <p className="mb-4">요청하신 페이지가 존재하지 않습니다.</p>
+      <Link
+        href="/"
+        className="text-primary_100 hover:underline"
+      >
+        메인페이지로 돌아가기
+      </Link>
+    </div>
   );
-};
-
-export default NotFoundPage;
+}
