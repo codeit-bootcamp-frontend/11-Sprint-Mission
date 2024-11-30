7fc9edb1df3382ddc65897c656dd1426d734127a
WooLegend <dnwogus4260@naver.com>
2024-12-01 02:50:57 +0900

    feat: 아이디 기반 게시글 읽기 API 구현

--- api/article.api.tsx
+++ api/article.api.tsx
@@ -1,4 +1,4 @@
-import { ArticleList } from "@/types/Article.type";
+import { Article, ArticleList } from "@/types/Article.type";
 import axios from "./axios";
 
 interface GetArticleListParams {
@@ -27,6 +27,11 @@ async function getArticleList({
   return response.data;
 }
 
+async function getArticle({ id }: { id: number }): Promise<Article> {
+  const response = await axios.get(`/articles/${id}`);
+  return response.data;
+}
+
 interface PostArticle {
   image?: string;
   content: string;
@@ -43,5 +48,5 @@ async function postArticle({ image, content, title }: PostArticle) {
   });
 }
 
-export { getArticleList, postArticle };
+export { getArticleList, getArticle, postArticle };
 export type { GetArticleListParams, OrderBy };
