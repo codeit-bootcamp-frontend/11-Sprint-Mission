67469ebc415332ac49735f53626d4644a611a08d
장수경 <jangsookyeong@jangsoogyeong-ui-MacBookAir.local>
2024-11-30 21:38:05 +0900

    ArticleContent 수정

--- components/board/ArticleContent.tsx
+++ components/board/ArticleContent.tsx
@@ -1,1 +1,30 @@
+import { Article } from "@/types/Types";
+import Kebob from "@/public/images/ic_kebab.svg";
+import ArticleInfo from "./ArticleInfo";
 
+interface ArticleContentProps {
+  article: Article;
+}
+
+const ArticleContent = ({ article }: ArticleContentProps) => {
+  return (
+    <div>
+      <div>
+        <h3>{article.title}</h3>
+
+        <button>
+          <Kebob />
+        </button>
+
+        <div>
+          <ArticleInfo article={article} />
+          {/* 하트 */}
+        </div>
+      </div>
+
+      <div>{article.content}</div>
+    </div>
+  );
+};
+
+export default ArticleContent;
