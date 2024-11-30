3ad4d85ad513f8860140a66a19bd7757455565b2
WooLegend <dnwogus4260@naver.com>
2024-12-01 05:03:59 +0900

    feat: 게시글 ID 기반 댓글 목록 일기 API 구현

---
+++ api/comment.api.tsx
@@ -0,0 +1,25 @@
+import axios from "./axios";
+
+interface GetCommentListByArticleId {
+  articleId: number;
+  limit?: number;
+  cursor?: number;
+}
+
+async function getCommentListByArticleId({
+  articleId,
+  limit,
+  cursor,
+}: GetCommentListByArticleId) {
+  const response = await axios.get(`/articles/${articleId}/comments`, {
+    params: {
+      articleId,
+      limit,
+      cursor,
+    },
+  });
+  return response.data;
+}
+
+export { getCommentListByArticleId };
+export type { GetCommentListByArticleId };
