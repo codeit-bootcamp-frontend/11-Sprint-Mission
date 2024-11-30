c8b3642e6a5ddec0edc8911d28cacc8d429765dc
WooLegend <dnwogus4260@naver.com>
2024-12-01 04:19:37 +0900

    feat: 게시글 상세 페이지 헤더 및 메인 구현

--- pages/board/[id].tsx
+++ pages/board/[id].tsx
@@ -1,5 +1,10 @@
 import { getArticle } from "@/api/article.api";
 import { Article } from "@/types/Article.type";
+import Image from "next/image";
+import styles from "@/styles/article.module.css";
+import { useEffect, useRef, useState } from "react";
+import useOutsideClick from "@/hooks/useOutsideClick";
+import formatDate from "@/lib/formatDate";
 
 export async function getServerSideProps(context: any) {
   const { id } = context.params;
@@ -17,9 +22,71 @@ export default function ArticleDetail({ article }: { article: Article }) {
   if (!article) return null;
 
   return (
-    <div>
-      <h2>{article.title}</h2>
-      <p>{article.content}</p>
+    <>
+      <header className={styles.header}>
+        <div className={styles.wrap}>
+          <h2 className={styles.title}>{article.title}</h2>
+          <Dropdown />
+        </div>
+        <div className={styles.wrap}>
+          <div className={styles.info}>
+            <div className={styles.profile}>
+              <Image fill src="/images/profile.svg" alt="프로필" />
+            </div>
+            <span className={styles.writer}>{article.writer.nickname}</span>
+            <span className={styles.createdAt}>
+              {formatDate(article.createdAt)}
+            </span>
+          </div>
+          <div className={styles.likeCount}>
+            <div className={styles.heart}>
+              <Image fill src="/images/ic_heart.svg" alt="좋아요" />
+            </div>
+            <span>{article.likeCount}</span>
+          </div>
+        </div>
+      </header>
+      <main className={styles.content}>{article.content}</main>
+    </>
+  );
+}
+
+function Dropdown() {
+  const [selectedDropdown, setSelecedDropdown] = useState(false);
+  const dropdownRef = useRef<HTMLDivElement>(null);
+  const { flag } = useOutsideClick(dropdownRef);
+
+  const handleClickDropdown = () => setSelecedDropdown((prev) => !prev);
+
+  const handleClickOption = (event: React.MouseEvent<HTMLDivElement>) => {
+    const target = event.target;
+    if (!(target instanceof HTMLElement)) return;
+    if (!target.dataset.option) return;
+    // onChange(target.dataset.option as OrderBy);
+  };
+
+  useEffect(() => {
+    setSelecedDropdown(!flag);
+  }, [flag]);
+
+  return (
+    <div
+      className={styles.Dropdown}
+      onClick={handleClickDropdown}
+      ref={dropdownRef}
+    >
+      <Image fill src="/images/ic_kebab.svg" alt="기능" />
+
+      {selectedDropdown && (
+        <div className={styles.optionWrap} onClick={handleClickOption}>
+          <div className={styles.option} data-option="edit">
+            수정하기
+          </div>
+          <div className={styles.option} data-option="delete">
+            삭제하기
+          </div>
+        </div>
+      )}
     </div>
   );
 }
