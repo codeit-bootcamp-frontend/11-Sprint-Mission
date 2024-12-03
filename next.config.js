774a70fce8b0e2a5c7d9433bb4157068515c29f8
WooLegend <dnwogus4260@naver.com>
2024-12-03 11:23:49 +0900

    feat: next.config.js redirects 설정 추가

--- next.config.js
+++ next.config.js
@@ -23,6 +23,15 @@ const nextConfig = {
       ],
     },
   },
+  async redirects() {
+    return [
+      {
+        source: "/:path*",
+        destination: "/",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
