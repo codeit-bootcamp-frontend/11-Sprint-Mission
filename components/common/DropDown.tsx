0a43515b221d35d170f66dc7c9fdab843fd78b2c
ksj <naver.daum0711@gmail.com>
2024-12-08 20:02:20 +0900

    feat: dropdown 추가 중(기능 작동 이전)

--- components/common/DropDown.tsx
+++ components/common/DropDown.tsx
@@ -1,3 +1,29 @@
+import { useState } from "react";
+import styles from "@/components/common/DropDown.module.css";
+import Image from "next/image";
+import Sort from "@/assets/images/icons/ic_sort.svg";
+
 export default function DropDown() {
-  return <>filter</>;
+  const [view, setView] = useState(false);
+  // dropdown 만들 때 button 태그, ul 태그 등 방법이 여러 개인 거 같던데 차이점이 무엇인지?
+  return (
+    <ul
+      className={styles.dropdown}
+      onClick={() => {
+        setView(!view);
+      }}
+    >
+      <span className={styles.sort}>
+        최신순
+        <Image
+          src={Sort}
+          alt="정렬 아이콘"
+          width={24}
+          height={24}
+          className={view ? styles.rotation : ""}
+        />
+      </span>
+      {view && <li>기본순</li>}
+    </ul>
+  );
 }
